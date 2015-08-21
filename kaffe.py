7c0171848d5c472c63017f5442693b406883d364
Karl Zipser <karlzipser@berkeley.edu>
2015-08-20 21:12:46 -0700

    gacp

--- kaffe.py
+++ kaffe.py
@@ -82,6 +82,40 @@ def make_step(net, step_size=1.5, end='inception_4c/output',
         bias = net.transformer.mean['data']
         src.data[:] = np.clip(src.data, -bias, 255-bias)    
 
+def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, 
+              end='inception_4c/output', clip=True, **step_params):
+    # prepare base images for all octaves
+    octaves = [preprocess(net, base_img)]
+    for i in xrange(octave_n-1):
+        octaves.append(nd.zoom(octaves[-1], (1, 1.0/octave_scale,1.0/octave_scale), order=1))
+    
+    src = net.blobs['data']
+    detail = np.zeros_like(octaves[-1]) # allocate image for network-produced details
+    for octave, octave_base in enumerate(octaves[::-1]):
+        h, w = octave_base.shape[-2:]
+        if octave > 0:
+            # upscale details from the previous octave
+            h1, w1 = detail.shape[-2:]
+            detail = nd.zoom(detail, (1, 1.0*h/h1,1.0*w/w1), order=1)
+
+        src.reshape(1,3,h,w) # resize the network's input image size
+        src.data[0] = octave_base+detail
+        for i in xrange(iter_n):
+            make_step(net, end=end, clip=clip, **step_params)
+            
+            # visualization
+            vis = deprocess(net, src.data[0])
+            if not clip: # adjust image contrast if clipping is disabled
+                vis = vis*(255.0/np.percentile(vis, 99.98))
+            #showarray(vis)
+            #print octave, i, end, vis.shape
+            clear_output(wait=True)
+            
+        # extract details produced on the current octave
+        detail = src.data[0]-octave_base
+    # returning the resulting image
+    return deprocess(net, src.data[0])
+
 
 def do_it(num_nodes,layer):
     img_path = opj(home_path,'scratch/2015/8/19/bvlc_reference_caffenet/'+layer)
@@ -137,7 +171,7 @@ def do_it2(layer,net,iter_n):
         #mi(img,opj(img_path,str(n)+'.png'))
         imsave(opj(img_path,str(n)+'.png'),img)
 
-def do_it3(layer,net,iter_n):
+def do_it3(layer,net,iter_n,start=0):
 
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2,0,1))
@@ -148,9 +182,9 @@ def do_it3(layer,net,iter_n):
     layer_shape=list(np.shape(net.blobs[layer].data));
     layer_shape[0] = 1
     layer_shape = tuple(layer_shape)
-    img_path = opj(home_path,'scratch/2015/8/18/bvlc_reference_caffenet/'+layer)
+    img_path = opj(home_path,'scratch/2015/8/20/bvlc_googlenet/'+layer.replace('/','-'))
     unix('mkdir -p ' + img_path)
-    for n in range(layer_shape[1]):#(num_nodes):
+    for n in range(start,layer_shape[1]):#(num_nodes):
         mask7 = np.zeros(layer_shape)
         #n = np.random.randint(1000)
         mask7[:,n] = 1.0
@@ -166,12 +200,13 @@ def do_it3(layer,net,iter_n):
             net.blobs['data'].data[0][1,:,:] = 1.0*net.blobs['data'].data[0][0,:,:]
             net.blobs['data'].data[0][2,:,:] = 1.0*net.blobs['data'].data[0][0,:,:]
 
-        #pb = ProgressBar(iter_n)
+        pb = ProgressBar(iter_n)
         for i in range(iter_n):
             make_step(net,end=layer,objective=objective_kz7)
             src = net.blobs['data']
             #vis = deprocess(net, src.data[0])
-            #pb.animate(i+1)
+            if np.mod(i,10.0)==0:
+                pb.animate(i+1)
         print((model_folders[MODEL_NUM],layer,n))#,labels[n]))
         vis = deprocess(net, src.data[0])
         img = np.uint8(np.clip(vis, 0, 255))
@@ -191,7 +226,7 @@ def do_it4(layer,net,iter_n):
     layer_shape = tuple(layer_shape)
     img_path = opj(home_path,'scratch/2015/8/19/bvlc_reference_caffenet/'+layer)
     unix('mkdir -p ' + img_path)
-    for n in range(5):#layer_shape[1]):#(num_nodes):
+    for n in range(start,layer_shape[1]):#(num_nodes):
         mask7 = np.zeros(layer_shape)
         #n = np.random.randint(1000)
         xy = np.int(layer_shape[2]/2)
@@ -224,7 +259,7 @@ def do_it4(layer,net,iter_n):
 
 #############################
 
-MODEL_NUM = 2
+MODEL_NUM = 0
 net = get_net(MODEL_NUM)
 
 print(np.shape(net.blobs['data'].data))
@@ -232,8 +267,8 @@ src = net.blobs['data']
 src.reshape(1,3,227,227)
 print(np.shape(net.blobs['data'].data))
 
-for l in ['conv1','conv2','conv3','conv4','conv5']:
-    do_it4(l,net,100)
+for l in ['inception_5b/5x5']:
+    do_it3(l,net,1000,0)
 #do_it3('conv3',net,100)
 
 
