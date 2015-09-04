414ccd728ab1cf44e50e11953b2833fecf0cfa60
Karl Zipser <karlzipser@hiMac.local>
2015-09-04 08:19:23 -0700

    gacp

---
+++ thean.py
@@ -0,0 +1,134 @@
+from kzpy3.utils import *
+
+import cPickle
+import gzip
+import timeit
+
+import theano
+import theano.tensor as T
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+
+
+class InnerProductLayer(object):
+    def __init__(self, rng, input, n_in, n_out, activation=T.tanh):
+        
+        W_values = np.asarray(
+                rng.uniform(
+                    low=-np.sqrt(6. / (n_in + n_out)),
+                    high=np.sqrt(6. / (n_in + n_out)),
+                    size=(n_in, n_out)
+                ),
+                dtype=theano.config.floatX
+            )
+        if activation == theano.tensor.nnet.sigmoid:
+            W_values *= 4
+
+        W = theano.shared(value=W_values, name='W', borrow=True)
+
+        b_values = np.zeros((n_out,), dtype=theano.config.floatX)
+        
+        b = theano.shared(value=b_values, name='b', borrow=True)
+
+            
+            
+        self.W = W
+        self.b = b
+        
+        self.lin_output = T.dot(input, self.W) + self.b
+        self.params = [self.W, self.b]
+        self.input = input
+        self.output = (
+            self.lin_output if activation is None
+            else activation(self.lin_output)
+        )
+        self.output_argmax = T.argmax(self.output, axis=0)
+
+    def mean_sqr_error(self, y):
+        return T.mean(T.sqr(self.output - y))
+
+
+class LeNetConvPoolLayer(object):
+    """Pool Layer of a convolutional network """
+
+    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
+        """
+        Allocate a LeNetConvPoolLayer with shared variable internal parameters.
+
+        :type rng: np.random.RandomState
+        :param rng: a random number generator used to initialize weights
+
+        :type input: theano.tensor.dtensor4
+        :param input: symbolic image tensor, of shape image_shape
+
+        :type filter_shape: tuple or list of length 4
+        :param filter_shape: (number of filters, num input feature maps,
+                              filter height, filter width)
+
+        :type image_shape: tuple or list of length 4
+        :param image_shape: (batch size, num input feature maps,
+                             image height, image width)
+
+        :type poolsize: tuple or list of length 2
+        :param poolsize: the downsampling (pooling) factor (#rows, #cols)
+        """
+
+        assert image_shape[1] == filter_shape[1]
+        self.input = input
+
+        # there are "num input feature maps * filter height * filter width"
+        # inputs to each hidden unit
+        fan_in = np.prod(filter_shape[1:])
+        # each unit in the lower layer receives a gradient from:
+        # "num output feature maps * filter height * filter width" /
+        #   pooling size
+        fan_out = (filter_shape[0] * np.prod(filter_shape[2:]) /
+                   np.prod(poolsize))
+        # initialize weights with random weights
+        W_bound = np.sqrt(6. / (fan_in + fan_out))
+        self.W = theano.shared(
+            np.asarray(
+                rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
+                dtype=theano.config.floatX
+            ),
+            borrow=True
+        )
+        self.L1 = (
+            abs(self.W).sum()
+        )
+        self.L2_sqr = (
+            (self.W ** 2).sum()
+        )
+
+        
+        # the bias is a 1D tensor -- one bias per output feature map
+        b_values = np.zeros((filter_shape[0],), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, borrow=True)
+
+        # convolve input feature maps with filters
+        conv_out = conv.conv2d(
+            input=input,
+            filters=self.W,
+            filter_shape=filter_shape,
+            image_shape=image_shape
+        )
+
+        # downsample each feature map individually, using maxpooling
+        pooled_out = downsample.max_pool_2d(
+            input=conv_out,
+            ds=poolsize,
+            ignore_border=True
+        )
+
+        # add the bias term. Since the bias is a vector (1D array), we first
+        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
+        # thus be broadcasted across mini-batches and feature map
+        # width & height
+        self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+
+        # store parameters of this layer
+        self.params = [self.W, self.b]
+
+        # keep track of model input
+        self.input = input
+
