512b5584fc27507c6d85e164c674ec0b7384e6a8
Karl Zipser <karlzipser@hiMac.local>
2015-09-09 23:17:11 -0700

    gacp

--- caf/kaffe.py
+++ caf/kaffe.py
@@ -182,7 +182,7 @@ def do_it3(layer,net,iter_n,start=0):
     layer_shape=list(np.shape(net.blobs[layer].data));
     layer_shape[0] = 1
     layer_shape = tuple(layer_shape)
-    img_path = opj(home_path,'scratch/2015/9/8/'+model_folders[MODEL_NUM]+'/'+layer.replace('/','-'))
+    img_path = opj(home_path,'scratch/2015/9/9/'+model_folders[MODEL_NUM]+'/'+layer.replace('/','-'))
     unix('mkdir -p ' + img_path)
     for n in range(start,layer_shape[1]):#(num_nodes):
         mask7 = np.zeros(layer_shape)
@@ -279,5 +279,5 @@ if True:
     src.reshape(1,3,227,227)
     print(np.shape(net.blobs['data'].data))
 
-    for l in ['prob']:
-        do_it3(l,net,2000,130)
+    for l in ['fc6']:
+        do_it3(l,net,2000,0)
