c0617d1931dc6615aa9a4b614e446992b2e7f317
Karl Zipser <karlzipser@berkeley.edu>
2015-09-23 04:49:42 +0200

    gacp

--- caf/temp.py
+++ caf/temp.py
@@ -4,83 +4,6 @@ CS_('''
 *** Look at the progress of training person_clothing_bigger_18Sept2015 ***
 This network has conv1 four times bigger and conv2 two times bigger
 
-''')
-
-
-#current_model = 'person_clothing_bigger_18Sept2015'
-current_model = 'bvlc_googlenet_person'
-
-
-
-
-CS_('''training loss''')
-if current_model == 'person_clothing_bigger_18Sept2015':
-	ns = txt_file_to_list_of_strings(opjh('caffe/models/'+current_model+'/slurm-434550.out'))
-	ns += txt_file_to_list_of_strings(opjh('caffe/models/'+current_model+'/slurm-434875.out'))
-if current_model == 'bvlc_googlenet_person':
-	ns = txt_file_to_list_of_strings(opjh('caffe/models/bvlc_googlenet_person/slurm-435348.out'))
-	ns += txt_file_to_list_of_strings(opjh('caffe/models/bvlc_googlenet_person/slurm-435350.out'))
-
-PP[FF]=4,2
-plt.figure(d2n(current_model,''': loss'''))
-
-
-st = 'Train net output #0: loss1/loss1 = '
-train_loss = []
-for n in ns:
-    if st in n:
-        train_loss.append(float(n.split(st)[1].split()[0]))
-plot(train_loss[1:],'.');
-
-st = 'Train net output #1: loss2/loss1 = '
-train_loss = []
-for n in ns:
-    if st in n:
-        train_loss.append(float(n.split(st)[1].split()[0]))
-plot(train_loss[1:],'.');
-
-st = 'Train net output #2: loss3/loss3 = '
-train_loss = []
-for n in ns:
-    if st in n:
-        train_loss.append(float(n.split(st)[1].split()[0]))
-plot(train_loss[1:],'.');
-
-
-
-
-
-
-
-# Make sure that caffe is on the python path:
-caffe_root = opjh('caffe')  # this file is expected to be in {caffe_root}/examples
-import sys
-sys.path.insert(0, caffe_root + 'python')
-
-import caffe
-caffe.set_mode_cpu()
-
-model_to_load = False
-cms = gg(opjh('caffe/models/'+current_model+'/*.caffemodel'))
-if len(cms) > 0:
-    ctimes = []
-    for c in cms:
-        ctimes.append(os.path.getctime(c))
-    cms = sorted(cms,key=natural_keys)
-    ctimes = []
-    for c in cms:
-        ctimes.append(os.path.getctime(c))
-    sorted_indicies = np.argsort(np.array(ctimes))
-    model_to_load = cms[sorted_indicies[-1]]
-    print(d2s('***** model to load =',model_to_load))
-
-
-net = caffe.Net(opjh('caffe/models/'+current_model+'/deploy.prototxt'),model_to_load, caffe.TEST)
-
-#net = caffe.Net(opjh('caffe/models/bvlc_reference_caffenet/deploy.prototxt'),
-#                opjh('caffe/models/bvlc_reference_caffenet/model.caffemodel' ), #'caffe/models/bvlc_reference_caffenet/model.caffemodel'),
-#                caffe.TEST)
-
 def vis_square(data, fig_name='vis_square',subplot_array=[1,1,1], padsize=1, padval=0):
     data -= data.min()
     data /= data.max()   
@@ -94,6 +17,82 @@ def vis_square(data, fig_name='vis_square',subplot_array=[1,1,1], padsize=1, pad
     mi(data,fig_name,subplot_array)
     return data
 
+''')
+
+
+ 
+for current_model in ['bvlc_googlenet_person','person_clothing_bigger_18Sept2015']:
+
+
+
+
+	CS_('''training loss''')
+	if current_model == 'person_clothing_bigger_18Sept2015':
+		ns = txt_file_to_list_of_strings(opjh('caffe/models/'+current_model+'/slurm-434550.out'))
+		ns += txt_file_to_list_of_strings(opjh('caffe/models/'+current_model+'/slurm-434875.out'))
+	if current_model == 'bvlc_googlenet_person':
+		ns = txt_file_to_list_of_strings(opjh('caffe/models/bvlc_googlenet_person/slurm-435348.out'))
+		ns += txt_file_to_list_of_strings(opjh('caffe/models/bvlc_googlenet_person/slurm-435350.out'))
+
+	PP[FF]=4,2
+	plt.figure(d2n(current_model,''': loss'''))
+
+
+	st = 'Train net output #0: loss1/loss1 = '
+	train_loss = []
+	for n in ns:
+	    if st in n:
+	        train_loss.append(float(n.split(st)[1].split()[0]))
+	plot(train_loss[1:],'.');
+
+	st = 'Train net output #1: loss2/loss1 = '
+	train_loss = []
+	for n in ns:
+	    if st in n:
+	        train_loss.append(float(n.split(st)[1].split()[0]))
+	plot(train_loss[1:],'.');
+
+	st = 'Train net output #2: loss3/loss3 = '
+	train_loss = []
+	for n in ns:
+	    if st in n:
+	        train_loss.append(float(n.split(st)[1].split()[0]))
+	plot(train_loss[1:],'.');
+
+
+
+
+
+
+
+	# Make sure that caffe is on the python path:
+	caffe_root = opjh('caffe')  # this file is expected to be in {caffe_root}/examples
+	import sys
+	sys.path.insert(0, caffe_root + 'python')
+
+	import caffe
+	caffe.set_mode_cpu()
+
+	model_to_load = False
+	cms = gg(opjh('caffe/models/'+current_model+'/*.caffemodel'))
+	if len(cms) > 0:
+	    ctimes = []
+	    for c in cms:
+	        ctimes.append(os.path.getctime(c))
+	    cms = sorted(cms,key=natural_keys)
+	    ctimes = []
+	    for c in cms:
+	        ctimes.append(os.path.getctime(c))
+	    sorted_indicies = np.argsort(np.array(ctimes))
+	    model_to_load = cms[sorted_indicies[-1]]
+	    print(d2s('***** model to load =',model_to_load))
+
+
+	net = caffe.Net(opjh('caffe/models/'+current_model+'/deploy.prototxt'),model_to_load, caffe.TEST)
+
+	#net = caffe.Net(opjh('caffe/models/bvlc_reference_caffenet/deploy.prototxt'),
+	#                opjh('caffe/models/bvlc_reference_caffenet/model.caffemodel' ), #'caffe/models/bvlc_reference_caffenet/model.caffemodel'),
+	#                caffe.TEST)
 
 
 
@@ -103,19 +102,21 @@ def vis_square(data, fig_name='vis_square',subplot_array=[1,1,1], padsize=1, pad
 
 
 
-CS_('''Latest weights.''')
-if current_model == 'person_clothing_bigger_18Sept2015':
-	filters_b = net.params['conv1'][0].data.copy()
-	PP[FF] = 8,4
-if current_model == 'bvlc_googlenet_person':
-	filters_b = net.params['conv1/7x7_s2'][0].data.copy()
-	PP[FF] = 4,2
 
 
-vis_square(filters_b.transpose(0, 2, 3, 1)[:,:,:,:],d2n(current_model,''': latest weights.'''),[1,2,1])
-f = filters_b.copy()
+	CS_('''Latest weights.''')
+	if current_model == 'person_clothing_bigger_18Sept2015':
+		filters_b = net.params['conv1'][0].data.copy()
+		PP[FF] = 8,4
+	if current_model == 'bvlc_googlenet_person':
+		filters_b = net.params['conv1/7x7_s2'][0].data.copy()
+		PP[FF] = 4,2
 
-for i in range(shape(f)[0]):
-    f[i,:,:,:] = z2o(f[i,:,:,:])
-vis_square(f.transpose(0, 2, 3, 1),d2n(current_model,''': latest weights.'''),[1,2,2]);
-model_to_load
+
+	vis_square(filters_b.transpose(0, 2, 3, 1)[:,:,:,:],d2n(current_model,''': latest weights.'''),[1,2,1])
+	f = filters_b.copy()
+
+	for i in range(shape(f)[0]):
+	    f[i,:,:,:] = z2o(f[i,:,:,:])
+	vis_square(f.transpose(0, 2, 3, 1),d2n(current_model,''': latest weights.'''),[1,2,2]);
+	model_to_load
