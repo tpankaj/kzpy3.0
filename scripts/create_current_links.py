615e891177538e38c1e8d2abc820018901dbe193
Karl Zipser <karlzipser@berkeley.edu>
2015-10-08 05:28:43 -0700

    gacp

---
+++ scripts/create_current_links.py
@@ -0,0 +1,20 @@
+from kzpy3.utils import *
+
+d = datetime.date.today()
+y = str(d.year)
+m = str(d.month)
+
+mkcu = d2s('mkdir -p',opjh(y,m))
+cu = d2s('ln -s',opjh(y,m),'current')
+
+mkcgd = d2s('mkdir -p',opjh('Google_Drive',d2n(y,'-',m)))
+cgd = d2s('ln -s',opjh('Google_Drive',d2n(y,'-',m)),'current_GD')
+
+rmcu = d2s('rm',opjh('current'))
+rmcdg = d2s('rm',opjh('current_GD'))
+unix(rmcu,print_stdout=False, print_stderr=False,print_cmd=True)
+unix(rmcdg,print_stdout=False, print_stderr=False,print_cmd=True)
+unix(mkcu,print_stdout=False, print_stderr=False,print_cmd=True)
+unix(mkcgd,print_stdout=False, print_stderr=False,print_cmd=True)
+unix(cu,print_stdout=False, print_stderr=False,print_cmd=True)
+unix(cgd,print_stdout=False, print_stderr=False,print_cmd=True)
