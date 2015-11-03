fe7cb9c960d1227f4361168f04145d2c8b1ec940
Karl Zipser <karlzipser@berkeley.edu>
2015-11-03 15:31:47 -0800

    gacp

--- scratch/paramiko_test.py
+++ scratch/paramiko_test.py
@@ -2,5 +2,11 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('127.0.0.1', username='',password='')
-stdin,stdout,stderr = ssh.exec_command('pwd;cd ~/Desktop;pwd')
+stdin,stdout,stderr = ssh.exec_command('pwd;cd ~/Desktop;ls -al;pwd')
 print(stdout.read())
+
+import picamera
+camera = picamera.PiCamera()
+camera.hflip = True
+camera.vflip = True
+camera.capture('image1.png')
