407e7e45a93588c8f8c9756dd561b69701a27c2c
Karl Zipser <karlzipser@berkeley.edu>
2015-11-06 14:03:35 -0800

    gacp

---
+++ scratch/for_nino/rain_sandbox2.py
@@ -0,0 +1,29 @@
+"""
+animate timelapse frames matrix
+"""
+from kzpy3.vis import *
+from matplotlib.animation import FuncAnimation
+
+
+frames = load_img_folder_to_list(opjh('scratch/2015/10/8/timelapse.1444316394'))
+n_frames = len(frames)
+
+
+
+def even_frames(frame_number):
+	print(frame_number)
+	plt.clf()
+	mi(frames[frame_number])
+	if frame_number == n_frames-1:
+		plt.close()
+
+
+fig = plt.figure(1,figsize=(9,9))
+
+animation = FuncAnimation(fig, even_frames, frames=n_frames, interval=3, repeat=False)
+
+plt.show()
+
+print('done....')
+
+
