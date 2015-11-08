35641015cae3cd5ef2f75af716a28f9a05784858
Karl Zipser <karlzipser@berkeley.edu>
2015-11-07 23:22:20 -0800

    gacp

--- scratch/camera_control.py
+++ scratch/camera_control.py
@@ -23,7 +23,7 @@ ydim = 224
 camera.resolution = (np.int(1.3333*ydim), ydim)
 
 image_path = '/home/pi/image1.jpg'
-dst_image_path = '/Users/karlzipser/Desktop/image1.jpg'
+dst_image_path = '/Users/karlzipser/Desktop/RPi'
 
 camera_on_time = 600
 start_time = time.time()
@@ -40,7 +40,7 @@ while time.time() < start_time + camera_on_time:
 			print(d2s('Average interval =',time_sum / (1.0*ctr)))
 		last_time = t
 		camera.capture(image_path,format='jpeg', use_video_port=True,quality=10)
-		sftp.put(image_path, dst_image_path)
+		sftp.put(image_path, opj(dst_image_path,d2n(ctr,'.jpg'))
 	except:
 		break
     
--- scratch/view_RPi_images.py
+++ scratch/view_RPi_images.py
@@ -7,10 +7,24 @@ from matplotlib.animation import FuncAnimation
 
 n_frames = 3000
 
+c = 0
+ctr = 0
 
 def even_frames(frame_number):
+	global c
+	global ctr
 	#print(frame_number)
+
+	start_time = time.time()
 	try:
+		#c_new = os.path.getctime(opjD('image1.jpg'))
+		#if c_new == c:
+		#	pass #print('waiting...')
+		#else:
+		#c = c_new
+		#unix(d2n('cp ',opjD('image1.jpg '),' /Users/karlzipser/Desktop/RPi_images/',c_new,'.',ctr,'.jpg'),False)
+		#ctr += 1
+		#print ctr
 		img = imread(opjD('image1.jpg'))
 		#print(shape(img))
 		if shape(img)[2] == 3:
