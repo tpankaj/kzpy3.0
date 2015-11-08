bb9ae24bf91461f2609aab727cdb80504934801b
Karl Zipser <karlzipser@berkeley.edu>
2015-11-08 14:19:09 -0800

    gacp

--- scratch/gpio_control.py
+++ scratch/gpio_control.py
@@ -65,12 +65,13 @@ def onKeyPress(event):
     if event.char == 'q':
         GPIO.cleanup()
         root.destroy() #root.quit()
-
+"""
 root = tk.Tk()
 root.geometry('300x200')
 text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
 text.pack()
 root.bind('<KeyPress>', onKeyPress)
 #root.mainloop()
+"""
 
 
