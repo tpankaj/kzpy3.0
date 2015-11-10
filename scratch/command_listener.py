0d01e5d93953b77780c5153edfb5d6ff2f345015
Karl Zipser <karlzipser@berkeley.edu>
2015-11-09 21:31:41 -0800

    gacp

--- scratch/command_listener.py
+++ scratch/command_listener.py
@@ -3,11 +3,16 @@
 ipython --pylab osx kzpy3/scratch/keypress_view_RPi.py; reset
 
 ssh pi@192.168.43.20
-
+sudo python kzpy3/scratch/camera_control.py
 """
 
+import sys
+sys.path.insert(0, "/home/pi")
 from kzpy3.utils import *
+import RPi.GPIO as GPIO
 
+
+###############
 last_cmd ='no command yet'
 
 #command_file_path = '/Users/karlzipser/Desktop/distal_command.txt'
@@ -21,7 +26,15 @@ def update():
         if cmd_lst[0] != last_cmd:
             last_cmd = cmd_lst[0]
             print(last_cmd)
-            if last_cmd[0] == 'q':
+            if str_contains(last_cmd,'up'):
+                print('GO UP')
+            elif str_contains(last_cmd,'down'):
+                print('GO DOWN')
+            elif str_contains(last_cmd,'left'):
+                print('GO LEFT')
+            elif str_contains(last_cmd,'right'):
+                print('GO RIGHT')
+            elif last_cmd[0] == 'q':
                 print('Quitting now.')
                 sys.exit(1)
     except KeyboardInterrupt:
@@ -29,8 +42,9 @@ def update():
         sys.exit(1)
     except:
         pass
-    time.sleep(0.1)
+    time.sleep(0.05)
     
 
 while True:
 	update()
+#########
