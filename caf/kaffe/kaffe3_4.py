fbbf92ada31ca28bfad1262868eba33ec0629b14
Karl Zipser <karlzipser@hiMac.local>
2015-11-10 10:25:14 -0800

    gacp

--- caf/kaffe/kaffe3_4.py
+++ caf/kaffe/kaffe3_4.py
@@ -302,9 +302,9 @@ if True:
     else:
         print("*********** Using CPU ***********")
     img_dic = {}
-    start_node = 0
+    start_node = 24
     for n in range(start_node,start_node+1000):
-        for r in range(20):
+        for r in range(1):
             for x in [4]:
                 for y in [7]:
                     for l in ['loss3/classifier']:#['inception_4c/5x5']:#inception_layers:#['conv1/7x7_s2']:#['inception_5b/5x5']:#['inception_4b/5x5']:# 'inception_4b/5x5']:# 'inception_4d/5x5']:#['inception_4e/output']:#['fc8']:
@@ -314,5 +314,5 @@ if True:
                         #    single_rf = True
                         single_rf = False
                         
-                        do_it3(l,net,100,n,n+1,single_rf,True,x,y,'scratch/2015/11/10',img_dic,use_mona=True)
+                        do_it3(l,net,3000,n,n+1,single_rf,True,x,y,'scratch/2015/11/10',img_dic,use_mona=True)
 
