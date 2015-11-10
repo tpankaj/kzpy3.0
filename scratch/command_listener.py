17b575c7c3294f48759d93cd7ff4b5dba16c38a8
Karl Zipser <karlzipser@berkeley.edu>
2015-11-09 21:38:04 -0800

    gacp

--- scratch/command_listener.py
+++ scratch/command_listener.py
@@ -12,6 +12,57 @@ from kzpy3.utils import *
 import RPi.GPIO as GPIO
 
 
+SERVO_IN = 38
+MOTOR_IN = 40
+#HB_EN1 = 7
+#HB_IN1 = 12
+#HB_IN2 = 13
+
+out_pins = [SERVO_IN,MOTOR_IN]#[SERVO_IN, HB_EN1, HB_IN1, HB_IN2]
+
+GPIO.setmode(GPIO.BOARD)
+for p in out_pins:
+    GPIO.setup(p,GPIO.OUT)
+
+def do_pwm(pin,frequency,duration,duty_cycle):
+    pwm = GPIO.PWM(pin,frequency)
+    start_time = time.time()
+    pwm.start(duty_cycle)
+    while time.time() < start_time + duration:
+        pass;
+    pwm.stop()
+
+def servo(
+    duty_cycle=7.5,
+    durataion=0.2,
+    freqency=50,
+    pin=SERVO_IN):
+    """
+    """
+    do_pwm(pin,frequency,duration,duty_cycle)
+
+
+
+
+def onKeyPress(event):
+    text.insert('end', '%s ' % (event.char, ))
+    if event.char == '1':
+        do_pwm(38,50,0.3,7.8)
+    if event.char == '2':
+        do_pwm(38,50,0.3,9.5)
+    if event.char == '3':
+        do_pwm(38,50,0.3,11)
+    if event.char == '4':
+        do_pwm(40,50,0.3,7.20)
+    if event.char == '5':
+        do_pwm(40,50,0.3,7.18)
+    if event.char == 'q':
+        GPIO.cleanup()
+        root.destroy() #root.quit()
+
+
+
+
 ###############
 last_cmd ='no command yet'
 
@@ -27,18 +78,24 @@ def update():
             last_cmd = cmd_lst[0]
             print(last_cmd)
             if str_contains(last_cmd,'up'):
-                print('GO UP')
+                do_pwm(40,50,0.3,7.20)
+                print('forward')
             elif str_contains(last_cmd,'down'):
-                print('GO DOWN')
+                do_pwm(38,50,0.3,9.5)
+                print('straight')
             elif str_contains(last_cmd,'left'):
-                print('GO LEFT')
+                do_pwm(38,50,0.3,7.8)
+                print('left')
             elif str_contains(last_cmd,'right'):
-                print('GO RIGHT')
+                do_pwm(38,50,0.3,11)
+                print('right')
             elif last_cmd[0] == 'q':
                 print('Quitting now.')
+                GPIO.cleanup()
                 sys.exit(1)
     except KeyboardInterrupt:
         print('Quitting now.')
+        GPIO.cleanup()
         sys.exit(1)
     except:
         pass
