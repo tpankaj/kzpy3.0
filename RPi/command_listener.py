b479b4acd017f4f3618a85b27f0274a5948bc789
Karl Zipser <karlzipser@berkeley.edu>
2015-11-10 21:49:00 -0800

    gacp

--- RPi/command_listener.py
+++ RPi/command_listener.py
@@ -13,7 +13,7 @@ from kzpy3.utils import *
 import RPi.GPIO as GPIO
 
 import paramiko
-hup = txt_file_to_list_of_strings('/Users/karlzipser/pw_RPi.txt')# '/Users/karlzipser/pw_MacbookPro.txt') #
+hup = txt_file_to_list_of_strings('/home/pi/pw_MacbookPro.txt') 
 host = hup[0]
 port = 22
 transport = paramiko.Transport((host, port))
