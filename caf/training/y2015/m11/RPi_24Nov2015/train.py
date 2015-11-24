317f6b37809c3250cb246255f66d543e1a40154a
Karl Zipser <karlzipser@hiMac.local>
2015-11-23 17:50:42 -0800

    gacp

---
+++ caf/training/y2015/m11/RPi_24Nov2015/train.py
@@ -0,0 +1,88 @@
+'''
+20 Sept. 2015
+Do this:
+from kzpy3.caf.training.y2015.m9.from_mnist.original_with_isolayers.train import *
+
+
+from kzpy3.vis import *
+%matplotlib osx
+
+t=1
+d = solver.net.blobs['pydata'].data
+i1 = solver.net.blobs['ic1_out2'].data
+i2 = solver.net.blobs['ic2_out2'].data
+i3 = solver.net.blobs['ic3_out2'].data
+i4 = solver.net.blobs['ic11_out2'].data
+i5 = solver.net.blobs['ic12_out1'].data
+for m in range(10):
+n = m +10
+ii1 = i1[n,:].transpose(1,2,0)
+ii2 = i2[n,:].transpose(1,2,0)
+ii3 = i3[n,:].transpose(1,2,0)
+ii4 = i4[n,:].transpose(1,2,0)
+ii5 = i5[n,-3:].transpose(1,2,0)
+dd=dd = d[n,0,:,:]
+mi(dd,t,[6,10,m+1],img_title=d2s(int(solver.net.blobs['label'].data[n])))
+mi(ii1,t,[6,10,m+11])
+mi(ii2,t,[6,10,m+21],img_title=d2s(solver.net.blobs['ip1'].data[n,:].argmax()))
+mi(ii3,t,[6,10,m+31],img_title=d2s(solver.net.blobs['ip1'].data[n,:].argmax()))
+mi(ii4,t,[6,10,m+41],img_title=d2s(solver.net.blobs['ip2'].data[n,:].argmax()))
+mi(ii5,t,[6,10,m+51],img_title=d2s(solver.net.blobs['ip2'].data[n,:].argmax()))
+
+
+mi(solver.net.blobs['data'].data[0][1,:,:])
+'''
+
+from kzpy3.vis import *
+import caffe
+
+os.chdir(home_path) # this is for the sake of the train_val.prototxt
+
+global solver
+#global net
+
+
+def setup_solver():
+	global solver
+	solver = caffe.SGDSolver(opjh("kzpy3/caf/training/y2015/m11/RPi_24Nov2015/solver.prototxt"))
+	for l in [(k, v.data.shape) for k, v in solver.net.blobs.items()]:
+		print(l)
+	for l in [(k, v[0].data.shape) for k, v in solver.net.params.items()]:
+		print(l)
+
+
+setup_solver()
+solver.step(1)
+mi(solver.net.blobs['data'].data[0][1,:,:])
+"""
+def train():
+	solver.step(5000)
+
+
+def look_at_numbers():
+	solver.step(1)
+	mi(solver.net.blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28),'train')
+	print solver.net.blobs['label'].data[:8]
+	mi(solver.test_nets[0].blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), 'test')
+	print solver.test_nets[0].blobs['label'].data[:8]
+
+
+def restore():
+	solver.restore(opjh('scratch/2015/11/23/caffe/models/RPi_24Nov2015/model_iter_5000.solverstate'))
+
+
+def deploy(img_name = opjh('caffe/examples/images/7.jpg')):
+	global net
+	net = caffe.Net(opjh('kzpy3/caf/training/y2015/m11/RPi_24Nov2015/solver.prototxt/solver.prototxt'),
+		opjh('scratch/2015/11/23/caffe/models/RPi_24Nov2015/model_iter_5000.solverstate'),caffe.TEST)
+	net.blobs['data'].reshape(1,1,28,28)
+	img = scipy.misc.imread(img_name)
+	img = img[:,:,0] # this network wants grayscale images, no color channel.
+	net.blobs['data'].data[...] = img
+	out = net.forward()
+	print("Predicted class is #{}.".format(out['prob'].argmax()))
+	mi(img,img_name)
+
+if __name__ == '__main__':
+	all()
+""";
---
+++ caf/training/y2015/m11/RPi_24Nov2015/train_from_scratch_gpu_rot_conv.py
@@ -0,0 +1,63 @@
+from kzpy3.utils import *
+import os
+#os.chdir('..')
+import sys
+sys.path.insert(0, './python')
+
+import caffe
+import numpy as np
+###  from pylab import *
+#### %matplotlib inline
+
+# losses will also be stored in the log
+caffe.set_device(0)
+caffe.set_mode_gpu()
+# We create a solver that fine-tunes from a previously trained network.
+#solver = caffe.SGDSolver('/global/home/users/karlz/scratch/models_caffe/finetune_BarryLyndon_8Sept2015/solver.prototxt')
+#solver.net.copy_from('/global/home/users/karlz/scratch/models_caffe/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+solver = caffe.SGDSolver(opjh('caffe/models/person_clothing_17Sept2015/solver.prototxt'))
+cms = gg(opjh('caffe/models/person_clothing_17Sept2015/*.caffemodel'))
+cms = sorted(cms,key=natural_keys)
+last_iter = int(cms[-1].split('model_iter_')[-1].split('.')[0])
+model_to_load = last_iter(d2n('model_iter_',last_iter,'.caffemodel'))
+model_to_load = opjh('caffe/models/person_clothing_17Sept2015',model_to_load)
+print(model_to_load)
+exit()
+#solver.net.copy_from(model_to_load)
+# For reference, we also create a solver that does no finetuning.
+#scratch_solver = caffe.SGDSolver('models/finetune_flickr_style/solver.prototxt')
+
+niter = 500000
+train_loss = []
+accuracy_lst = []
+
+def rotate_average_filters(filters):
+    for i in range(len(filters)):
+        f = filters[i,:,:,:].transpose((1,2,0))
+        f = np.rot90(f,np.mod(i,4))
+        filters[i,:,:,:] = f.transpose((2,0,1))
+    for i in range(0,len(filters),4):
+        f = filters[i,:,:,:]
+        for j in range(1,4):
+            f += filters[i+j,:,:,:]
+        f /= 4.0
+        for j in range(4):
+            filters[i+j,:,:,:] = np.rot90(f.transpose((1,2,0)),-j).transpose((2,0,1))
+
+def average_biases(biases):
+    for i in range(0,len(biases),4):
+            biases[i:(i+4)] = biases[i:(i+4)].mean()
+
+for it in range(niter):
+    if True:
+        for k in ['conv1', 'conv2', 'conv3']:
+            rotate_average_filters(solver.net.params[k][0].data)
+            average_biases(solver.net.params[k][1].data)
+    try:
+        solver.step(1)  # SGD by Caffe
+    except:
+        pass
+print 'done'
+
+
+
