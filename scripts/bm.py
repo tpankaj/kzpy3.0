6dd9db1a09a00a9c3df7f37573682428280f30a0
Karl Zipser <karlzipser@hiMac.local>
2015-12-13 19:27:52 -0800

    gacp

---
+++ scripts/bm.py
@@ -0,0 +1,5 @@
+#! //anaconda/bin/python
+
+from kzpy3.utils import *
+
+print('bm.py: a script to do bookmarking of websites')
---
+++ scripts/nyt.py
@@ -0,0 +1,13 @@
+#! //anaconda/bin/python
+
+#import objc
+import applescript
+a = """if application "Google Chrome" is running then
+        tell application "Google Chrome" to make new window with properties {mode:"incognito"}
+    else
+        do shell script "open -a /Applications/Google\\\ Chrome.app --args --incognito"
+    end if
+
+    tell application "Google Chrome" to activate
+    open location "http://nytimes.com" """
+applescript.AppleScript(a).run()
