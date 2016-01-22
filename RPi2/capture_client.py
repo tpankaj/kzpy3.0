2c50d576080132f6179f8b9d7b488b6d64afbfbb
Karl Zipser <karlzipser@berkeley.edu>
2016-01-22 11:21:11 -0800

    gacp

--- RPi2/capture_client.py
+++ RPi2/capture_client.py
@@ -15,7 +15,7 @@ try:
     with picamera.PiCamera() as camera:
         camera.resolution = (300,225)#(640, 480) #(300,225)
         # Start a preview and let the camera warm up for 2 seconds
-        camera.start_preview()
+        #camera.start_preview()
         time.sleep(2)
 
         # Note the start time and construct a stream to hold image data
