95e4ed632f2b10eb617c146e250a8d69c3cb9752
Karl Zipser <karlzipser@berkeley.edu>
2016-01-23 14:05:35 -0800

    gacp

--- RPi2/keypress_rain.py
+++ RPi2/keypress_rain.py
@@ -1,5 +1,5 @@
 """
-ipython --pylab osx kzpy3/scratch/keypress.py
+ipython --pylab osx kzpy3/RPi2/keypress_rain.py
 """
 
 from kzpy3.vis import *
@@ -21,45 +21,46 @@ clientsocket.connect((host, port))
 
 
 
-
-# Create new Figure and an Axes which fills it. 
 fig = plt.figure(figsize=(1,1))
 
-
 def update(frame_number):
-    pass
+    try:
+        pass
+    except KeyboardInterrupt:
+        print('Quitting now.')
+        print('\nCleaning up.')
+        sftp.close()
+        transport.close()
+        print('Done.')
+        sys.exit(1)
+
+
 
 def onclick(event):
     print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(
         event.button, event.x, event.y, event.xdata, event.ydata)
 
-
 def on_key(event):
+    print "on_key"
+    
     print event.key
     clientsocket.send(event.key)
     if event.key == 'q':
         clientsocket.close()
-    	plt.clf()
-    	plt.close()
-    	fig.canvas.mpl_disconnect(cid)
-    	print('quit!!')
-    	sys.exit(1)
+        plt.clf()
+        plt.close()
+        fig.canvas.mpl_disconnect(cid)
+        print('quit!!')
+        sys.exit(1)
     else:
-    	print(event.key, event.xdata, event.ydata)
+        print(event.key, event.xdata, event.ydata)
 
-
-
-    
 
 
 
 cid = fig.canvas.mpl_connect('key_press_event', on_key)
-fig.canvas.mpl_connect('button_press_event', onclick)
-#fig.canvas.mpl_connect('motion_notify_event', onclick)
-
 
 animation = FuncAnimation(fig, update, interval=10)
-plt.ion()
 plt.show()
 
 
@@ -69,4 +70,3 @@ plt.show()
 a=input('afd')
 while True:
 	pass
-#fig.canvas.mpl_disconnect(cid)
