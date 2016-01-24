8b6adc8ffef6bb37546a639d8216de800df17517
Karl Zipser <karlzipser@berkeley.edu>
2016-01-23 21:28:59 -0800

    gacp

--- RPi2/RPi_server.py
+++ RPi2/RPi_server.py
@@ -43,7 +43,7 @@ while True:
             t = False
         if t:
             print(d2s('t =',t))
-            pwm_servo.ChangeDutyCycle(t)
+            pwm_servo.ChangeDutyCycle(t[0])
         else:
             print('['+buf+']')
         #print(d2s(t[0],t[1]))
