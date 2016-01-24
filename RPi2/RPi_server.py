cdc60edfe5701cae00a8f28d2717be8810af3469
Karl Zipser <karlzipser@berkeley.edu>
2016-01-23 21:19:13 -0800

    gacp

--- RPi2/RPi_server.py
+++ RPi2/RPi_server.py
@@ -3,6 +3,27 @@ print "RPi_server.py server side"
 from kzpy3.utils import *
 import socket
 
+try:
+    sys.path.insert(0, "/home/pi")
+    import RPi.GPIO as GPIO
+    SERVO_IN = 38
+    MOTOR_IN = 40
+    out_pins = [SERVO_IN,MOTOR_IN]
+    def gpio_setup():
+        print('gpio_setup')
+        GPIO.setmode(GPIO.BOARD)
+        for p in out_pins:
+            GPIO.setup(p,GPIO.OUT)
+    gpio_setup() 
+    pwm_motor = GPIO.PWM(40,50)
+    pwm_servo = GPIO.PWM(38,50)
+    pwm_motor.start(0)
+    pwm_servo.start(0)
+except:
+    print("*** not RPi ****")
+
+
+
 host = '0.0.0.0' # 'localhost'
 port = 5000
 
@@ -25,6 +46,10 @@ while True:
         #print(d2s(t[0],t[1]))
         #break
         if buf == 'q':
+            try:
+                GPIO.cleanup()
+            except:
+                print("*** not RPi ****")
             time.sleep(0.1)
             break
 
