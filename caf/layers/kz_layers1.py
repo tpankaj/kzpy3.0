08404c957cf1ad2c620d07b9d347f944339ccb34
Karl Zipser <karlzipser@hiMac.local>
2016-01-28 23:25:48 -0800

    gacp

--- caf/layers/kz_layers1.py
+++ caf/layers/kz_layers1.py
@@ -6,105 +6,106 @@ import caffe
 
 
 
-class SimpleLayer0(caffe.Layer):
-    """A layer that just multiplies by one."""
-
-    def setup(self, bottom, top):
-        pass
-
-    def reshape(self, bottom, top):
-        top[0].reshape(*bottom[0].data.shape)
-
-    def forward(self, bottom, top):
-        top[0].data[...] = 1.0 * bottom[0].data
-
-    def backward(self, top, propagate_down, bottom):
-        bottom[0].diff[...] = 1.0 * top[0].diff
 
 
-class SimpleLayer1(caffe.Layer):
-    """A layer that sometimes reverses contrast."""
-
-    def setup(self, bottom, top):
-        pass
-
-    def reshape(self, bottom, top):
-        top[0].reshape(*bottom[0].data.shape)
-
-    def forward(self, bottom, top):
-    	r = np.random.random()
-    	#print(r)
-    	if r < 0.5:
-        	top[0].data[...] = bottom[0].data - 0.5
-        else:
-        	top[0].data[...] = 1.0 - bottom[0].data - 0.5
-
-    def backward(self, top, propagate_down, bottom):
-        bottom[0].diff[...] = 1.0 * top[0].diff # don't know what this should be, but perhaps it doesn't matter for a data layer
 
 
-class SimpleLayer2(caffe.Layer):
-    """A layer that adds distractors."""
-
-    def setup(self, bottom, top):
-        pass
-
-    def reshape(self, bottom, top):
-        top[0].reshape(*bottom[0].data.shape)
-    
-    def forward(self, bottom, top):
-        tp = zeros((1,1,28,28))
-        n = 12
-        n2 = 10
-        for i in range(100):
-            top[0].data[i,:,:,:] = bottom[0].data[i,:,:,:] - 0.5
-            mx = top[0].data[i,:,:,:].max()
-            top[0].data[i,:,:,:n] += bottom[0].data[randint(100),:,:,-n:]
-            top[0].data[i,:,:,-n:] += bottom[0].data[randint(100),:,:,:n]
-            top[0].data[i,:,:n2,:] += bottom[0].data[randint(100),:,-n2:,:]
-            top[0].data[i,:,-n2:,:] += bottom[0].data[randint(100),:,:n2,:]
-            tp = top[0].data[i,:,:,:]
-            tp[tp>mx] = mx
 
 
-            #top[0].data[i,:,:,:] += np.random.random(shape(bottom[0].data[r,:,:,:]))
-    def backward(self, top, propagate_down, bottom):
-        bottom[0].diff[...] = 1.0 * top[0].diff # don't know what this should be, but perhaps it doesn't matter for a data layer
 
 
-class SimpleLayer3(caffe.Layer):
-    """A layer that addds distractcors, sometimes reverses contrast."""
 
-    def setup(self, bottom, top):
-        pass
+def get_sessions_dic(RPi_sessions_path):
+    sessions_dic,_ = dir_as_dic_and_list(RPi_sessions_path)
+    for k in sessions_dic:
+        sessions_dic[k] = {}
+        _,jpg_lst = dir_as_dic_and_list(opj(RPi_sessions_path,k,'jpg'))
+        sessions_dic[k]['name_list'] = jpg_lst
+        sessions_dic[k]['name_dic'] = {}
+        for f in sessions_dic[k]['name_list']:
+            sessions_dic[k]['name_dic'][f] = {}
+        for d in reversed(
+            txt_file_to_list_of_strings(opj(RPi_sessions_path,k,'session_list-'+k+'.txt'))):
+            f = d.split(' ')[0]
+            c = d.split(' ')[-4:]
+            if f in sessions_dic[k]['name_dic']:
+                for i in range(len(c)):
+                    c[i] = int(c[i])
+                sessions_dic[k]['name_dic'][f]['cmd'] = c
+                sessions_dic[k]['name_dic'][f]['img'] = opj(RPi_sessions_path,k,'jpg',f)
+    return sessions_dic
+def rnd_img_name(sessions_dic):
+    ses = sessions_dic.keys()
+    lses = len(ses)
+    k = ses[np.random.randint(lses)]
+    lnm = len(sessions_dic[k]['name_list'])
+    i = np.random.randint(lnm)
+    f = sessions_dic[k]['name_list'][i]
+    return (k,i,f)
+def get_img_lst_and_target_lst(sessions_dic):
+    success = False
+    while success == False:
+        try:
+            success = True
+            k,i,f = rnd_img_name(sessions_dic)
+            rng = range(i,i+20)
+            flst = []
+            clst = []
+            for j in rng:
+                f = sessions_dic[k]['name_list'][j]
+                c = sessions_dic[k]['name_dic'][f]['cmd']
+                if c[0] < 1:
+                    success = False
+                    #print 'Failed!!!'
+                    break
+                else:
+                    flst.append(f)
+                    clst.append(c)
+            if success:
+                #print 'success'
+                #for q in zip(flst,clst):
+                #    print q
+                img_lst = []
+                for f in flst[:9]:
+                    if type(sessions_dic[k]['name_dic'][f]['img']) == str:
+                        img = imread(sessions_dic[k]['name_dic'][f]['img'])
+                        sessions_dic[k]['name_dic'][f]['img'] = img
+                    img_lst.append(sessions_dic[k]['name_dic'][f]['img'])    
+        except:
+            success = False
+    target_lst = []
+    for c in clst:
+        t =  (c[3]/10.0 - 7.2) / (11.7-7.2)
+        if t < 0 or t > 1:
+            raise ValueError('!!!!!!!!!!!! t < 0 or t > 1')
+        target_lst.append(t)
+    return img_lst,target_lst
 
-    def reshape(self, bottom, top):
-        top[0].reshape(*bottom[0].data.shape)
-    
-    def forward(self, bottom, top):
-        tp = zeros((1,1,28,28))
-        n = 12
-        n2 = 10
-        for i in range(100):
-            top[0].data[i,:,:,:] = bottom[0].data[i,:,:,:] - 0.5
-            mx = top[0].data[i,:,:,:].max()
-            top[0].data[i,:,:,:n] += bottom[0].data[randint(100),:,:,-n:]
-            top[0].data[i,:,:,-n:] += bottom[0].data[randint(100),:,:,:n]
-            top[0].data[i,:,:n2,:] += bottom[0].data[randint(100),:,-n2:,:]
-            top[0].data[i,:,-n2:,:] += bottom[0].data[randint(100),:,:n2,:]
-            tp = top[0].data[i,:,:,:]
-            tp[tp>mx] = mx
-            if np.random.random(1)[0] > 0.5:
-                top[0].data[i,:,:,:] = mx-tp
+sessions_dic = get_sessions_dic(opjh('Desktop/RPi_data'))
 
-            #top[0].data[i,:,:,:] += np.random.random(shape(bottom[0].data[r,:,:,:]))
-    def backward(self, top, propagate_down, bottom):
-        bottom[0].diff[...] = 1.0 * top[0].diff # don't know what this should be, but perhaps it doesn't matter for a data layer
+mask = np.load(opjh('Desktop/mask.npy'))
+mask = mask[:,:,0]
 
-xy_offset = [0,0]
+
+
+
+
+
+
+
+
+
+
+ctr = 0
+last_time = time.time()
+
+
+
+target_lst = []
+
 class SimpleLayer4(caffe.Layer):
     """"""
-
+    
     def setup(self, bottom, top):
         pass
 
@@ -112,25 +113,34 @@ class SimpleLayer4(caffe.Layer):
         top[0].reshape(*bottom[0].data.shape)
     
     def forward(self, bottom, top):
-        #xy_offset[0] = np.random.randint(3)
-        #xy_offset[1] = np.random.randint(3)
-        img = imread(opjD('img.jpg'))
-        x1 = xy_offset[0]
+        global target_lst
+        global ctr,last_time,mask
+        if np.mod(ctr,10) == 0:
+            print time.time()-last_time
+            last_time = time.time()
+        img_lst,target_lst = get_img_lst_and_target_lst(sessions_dic)
+        x1 = 0
         x2 = x1+298
-        y1 = xy_offset[1]
+        y1 = 0
         y2 = y1+224
-        #print(shape(top[0].data))
-        #print(shape(img))
-        top[0].data[0,0,:,:] = img[:,:,2]
-        top[0].data[0,1,:,:] = img[:,:,1]
-        top[0].data[0,2,:,:] = img[:,:,0]
+        top[0].data[0,0,:,:] = (img_lst[0][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,1,:,:] = (img_lst[1][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,2,:,:] = (img_lst[2][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,3,:,:] = (img_lst[3][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,4,:,:] = (img_lst[4][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,5,:,:] = (img_lst[5][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,6,:,:] = (img_lst[6][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,7,:,:] = (img_lst[7][:,:,1]/255.0-0.5)*mask
+        top[0].data[0,8,:,:] = (img_lst[8][:,:,1]/255.0-0.5)*mask
+        ctr += 1
+
     def backward(self, top, propagate_down, bottom):
         bottom[0].diff[...] = 1.0 * top[0].diff # don't know what this should be, but perhaps it doesn't matter for a data layer
 
 
 class SimpleLayer5(caffe.Layer):
     """"""
-
+    
     def setup(self, bottom, top):
         pass
 
@@ -138,12 +148,11 @@ class SimpleLayer5(caffe.Layer):
         top[0].reshape(*bottom[0].data.shape)
     
     def forward(self, bottom, top):
-        tp = zeros((1,1,28,28))
-        top[0].data[0,:] = np.random.rand(11)
-        top[0].data[0,0] = xy_offset[0]
-        top[0].data[0,1] = xy_offset[1]
+        global target_lst
+        for i in range(len(target_lst)):
+            top[0].data[0,i] = target_lst[i]
 
-            #top[0].data[i,:,:,:] += np.random.random(shape(bottom[0].data[r,:,:,:]))
+
     def backward(self, top, propagate_down, bottom):
         bottom[0].diff[...] = 1.0 * top[0].diff # don't know what this should be, but perhaps it doesn't matter for a data layer
 
--- caf/training/y2016/m1/from_mnist/original_with_accuracy/train.py
+++ caf/training/y2016/m1/from_mnist/original_with_accuracy/train.py
@@ -13,6 +13,8 @@ Next steps:
 from kzpy3.caf.training.y2016.m1.from_mnist.original_with_accuracy.train import *;solver = setup_solver()
 solver.step(1000)
 
+solver.net.copy_from('/Users/karlzipser/scratch/2016/1/26/caffe/models/from_mnist/original_with_accuracy/model_iter_40000.caffemodel')
+
 '''
 
 from kzpy3.vis import *
@@ -32,17 +34,35 @@ def setup_solver():
 
 
 solver = setup_solver()
-solver.step(1)
 
-img = np.zeros((224,298,3))
-img[:,:,0] = solver.net.blobs['py_image_data'].data[0,2,:,:]
-img[:,:,1] = solver.net.blobs['py_image_data'].data[0,1,:,:]
-img[:,:,2] = solver.net.blobs['py_image_data'].data[0,0,:,:]
-mi(img/255.0)
-print( solver.net.blobs['py_target_data'].data[0])
+def show_solver_step(solver):
+	solver.step(1)
+	img = np.zeros((224,298,3))
+	img[:,:,0] = solver.net.blobs['py_image_data'].data[0,6,:,:]+0.5
+	img[:,:,1] = solver.net.blobs['py_image_data'].data[0,7,:,:]+0.5
+	img[:,:,2] = solver.net.blobs['py_image_data'].data[0,8,:,:]+0.5
+	plt.clf()
+	mi(img,1)
+	print( solver.net.blobs['py_target_data'].data[0])
+	print( solver.net.blobs['ip2'].data)
+	print( solver.net.blobs['identity'].data)
+	plt.figure(2)
+	plt.clf()
+	plt.plot([0,1],[0,1],'k')
+	plt.plot(solver.net.blobs['py_target_data'].data[0][:9],solver.net.blobs['ip2'].data[0][:9],'o')
+	plt.plot(solver.net.blobs['py_target_data'].data[0][9:],solver.net.blobs['ip2'].data[0][9:],'ro')
+	plt.xlim((0,1))
+	plt.ylim((0,1))
 
-#def train():
-#	solver.step(5000)
+show_solver_step(solver)
+
+
+def safe_solver_step(solver):
+    while True:
+        try:
+            solver.step(10000)
+        except Exception, e: 
+            print e
 
 
 def look_at_numbers(solver):
@@ -71,4 +91,4 @@ def deploy(img_name = opjh('caffe/examples/images/7.jpg')):
 	mi(img,img_name)
 	return net
 
-
+"""
