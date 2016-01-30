b748f3f040bb26b389925519ab0cde3ae40ead8a
Karl Zipser <karlzipser@berkeley.edu>
2016-01-29 22:21:04 -0800

    gacp

--- RPi2/button_test.py
+++ RPi2/button_test.py
@@ -7,7 +7,7 @@ from kzpy3.utils import *
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+#
 while True:
     input_state = GPIO.input(7)
     if input_state == False:
