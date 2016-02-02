079c03891c771bbb57c152d02baec4fe2a8fea5c
Karl Zipser <karlzipser@berkeley.edu>
2016-02-02 11:12:32 -0800

    gacp

--- RPi2/temp3.py
+++ RPi2/temp3.py
@@ -8,25 +8,44 @@ EN1 = 7
 IN1 = 12
 IN2 = 13
 out_pins = [EN1,IN1,IN2]
-def gpio_setup():
-    print('gpio_setup')
-    GPIO.setmode(GPIO.BOARD)
-    for p in out_pins:
-        GPIO.setup(p,GPIO.OUT)
-gpio_setup() 
-pwm_EN1 = GPIO.PWM(EN1,1)
-pwm_EN1.start(50)
-pwm_IN1 = GPIO.PWM(IN1,50)
-pwm_IN1.start(0)
-pwm_IN2 = GPIO.PWM(IN2,50)
-pwm_IN2.start(0)
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	for p in out_pins:
+		GPIO.setup(p,GPIO.OUT) 
+	pwm_EN1 = GPIO.PWM(EN1,50)
+	pwm_EN1.start(0)
+	pwm_IN1 = GPIO.PWM(IN1,50)
+	pwm_IN1.start(0)
+	pwm_IN2 = GPIO.PWM(IN2,50)
+	pwm_IN2.start(0)
+
+setup()
+
+def forward(ds=50):
+	pwm_IN1.ChangeDutyCycle(100)
+	pwm_IN2.ChangeDutyCycle(0)
+	pwm_EN1.ChangeDutyCycle(ds)
+
+def reverse(ds=50):
+	pwm_IN2.ChangeDutyCycle(100)
+	pwm_IN1.ChangeDutyCycle(0)
+	pwm_EN1.ChangeDutyCycle(ds)
+
+def stop():
+	pwm_IN2.ChangeDutyCycle(100)
+	pwm_IN1.ChangeDutyCycle(100)
+	pwm_EN1.ChangeDutyCycle(0)
+
 #
 ##############
 
-
+"""
 
 # http://stackoverflow.com/questions/1093598/pyserial-how-to-read-the-last-line-sent-from-a-serial-device
 import serial
 ser = serial.Serial('/dev/tty.usbmodem1411',9600)
 print ser.readline()
 ser.write('5')
+"""
+
