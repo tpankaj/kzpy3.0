d976de9daca1097bebe4641da0b99eda89a4fa14
Karl Zipser <karlzipser@berkeley.edu>
2016-02-03 11:07:18 -0800

    gacp

--- RPi2/range0.py
+++ RPi2/range0.py
@@ -32,29 +32,30 @@ GPIO.output(GPIO_TRIGGER, False)
 
 # Allow module to settle
 time.sleep(0.5)
+	for i in range(100):
+	# Send 10us pulse to trigger
+	GPIO.output(GPIO_TRIGGER, True)
+	time.sleep(0.00001)
+	GPIO.output(GPIO_TRIGGER, False)
+	start = time.time()
+	while GPIO.input(GPIO_ECHO)==0:
+	  start = time.time()
 
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
+	while GPIO.input(GPIO_ECHO)==1:
+	  stop = time.time()
 
-while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
+	# Calculate pulse length
+	elapsed = stop-start
 
-# Calculate pulse length
-elapsed = stop-start
+	# Distance pulse travelled in that time is time
+	# multiplied by the speed of sound (cm/s)
+	distance = elapsed * 34000
 
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distance = elapsed * 34000
+	# That was the distance there and back so halve the value
+	distance = distance / 2
 
-# That was the distance there and back so halve the value
-distance = distance / 2
-
-print "Distance : %.1f" % distance
-
+	print "Distance : %.1f cm" % distance
+	time.sleep(0.5)
+	
 # Reset GPIO settings
 GPIO.cleanup()
