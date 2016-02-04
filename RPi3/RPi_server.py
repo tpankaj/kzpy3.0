ab227cdaa970bcd80fc9ab6f7d74868416de28fe
Karl Zipser <karlzipser@berkeley.edu>
2016-02-04 11:23:39 -0800

    gacp

--- RPi3/RPi_server.py
+++ RPi3/RPi_server.py
@@ -37,7 +37,7 @@ port = 5000
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
 serversocket.listen(5) # become a server socket, maximum 5 connections
-TIMEOUT_DURATION = 1.0
+TIMEOUT_DURATION = 10.0
 connection, address = serversocket.accept()
 connection.settimeout(TIMEOUT_DURATION)
 #
--- RPi3/osx_client.py
+++ RPi3/osx_client.py
@@ -2,15 +2,20 @@
 
 To run:
 ssh pi@192.168.43.20
-sudo python kzpy3/RPi2/RPi_server.py
+sudo python kzpy3/RPi3/RPi_server.py
 sudo python kzpy3/RPi2/camera_control3.py
-ipython --pylab osx kzpy3/RPi2/osx_gui_client.py ; reset
+ipython kzpy3/RPi3/osx_client.py ; reset
 
 """
 
 from kzpy3.utils import *
-
 print "Client Side:"
+
+import serial
+ser = serial.Serial('/dev/tty.usbmodem1411',9600)
+
+
+
 import socket
 host = '192.168.43.20'
 #host = 'localhost'
@@ -18,7 +23,14 @@ port = 5000
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect((host, port))
 
+sent_t = time.time()
+send_wait = 0.1
+
 while True:
-    clientsocket.send(d2s(time.time()))
-    time.sleep(0.1)
+    s = ser.readline()
+    t = time.time()
+    if t - sent_t > send_wait:
+        sent_t = t
+        clientsocket.send(d2s(t))
+        #print(s)
 
