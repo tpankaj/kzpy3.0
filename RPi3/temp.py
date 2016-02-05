d9a0c2e36581db9d42067d756c0fbfa1c2cc6c7e
Karl Zipser <karlzipser@berkeley.edu>
2016-02-04 21:21:48 -0800

    gacp

--- RPi3/temp.py
+++ RPi3/temp.py
@@ -7,10 +7,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(31,GPIO.OUT)
 
 pwm_steer = GPIO.PWM(31,50)
+
+pwm_steer.start(0)
+
 pwm_steer.ChangeDutyCycle(7.8)
 pwm_steer.ChangeDutyCycle(0)
-pwm_steer.start(0)
-for nino in np.arange(2.2,10,0.1):
+print('centered at 7.8 [?]')
+time.sleep(3)
+
+for nino in np.arange(2.2,8,0.0333):
     pwm_steer.ChangeDutyCycle(nino)
     time.sleep(0.1);pwm_steer.ChangeDutyCycle(0);time.sleep(0.3)
     print nino
