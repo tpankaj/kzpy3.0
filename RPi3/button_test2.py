2cd9746071fb0a3fa6b7005030abdeef03f2ac72
Karl Zipser <karlzipser@berkeley.edu>
2016-02-06 09:46:10 -0800

    gacp

--- RPi3/button_test2.py
+++ RPi3/button_test2.py
@@ -7,24 +7,20 @@ from kzpy3.utils import *
 GPIO.setmode(GPIO.BOARD)
 pin = input('Enter GPIO pin: ')
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#
+
+button_press = 0
 def my_callback(channel):  
+	global button_press
     if GPIO.input(pin):     # if port 25 == 1  
         print "Rising edge detected"  
     else:                  # if port 25 != 1  
-        print "Falling edge detected"  
+        print "Falling edge detected" 
+        button_press += 1 
   
 # when a changing edge is detected on port 25, regardless of whatever   
 # else is happening in the program, the function my_callback will be run  
 GPIO.add_event_detect(pin, GPIO.BOTH, callback=my_callback)  
 
-while True:
-    input_state = GPIO.input(pin)
-    if input_state == False:
-    	time.sleep(3)
-        print('Button Pressed')
-        GPIO.cleanup()
-        #unix('sudo shutdown -h -P 0')
-        break
-    time.sleep(0.1)
+while button_press < 5:
     print(time.time())
+    time.sleep(1)
