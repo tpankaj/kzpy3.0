1ab3bb495dc5bab84c9b16931901e6a260c55bc5
Karl Zipser <karlzipser@berkeley.edu>
2016-02-06 09:25:58 -0800

    gacp

--- RPi3/button_test.py
+++ RPi3/button_test.py
@@ -5,11 +5,11 @@ sys.path.insert(0, "/home/pi")
 from kzpy3.utils import *
 
 GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+pin = input('Enter GPIO pin: ')
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #
 while True:
-    input_state = GPIO.input(7)
+    input_state = GPIO.input(pin)
     if input_state == False:
         print('Button Pressed')
         GPIO.cleanup()
