79fc40bf41e41293aecb8e428a1fd3614de04fd4
Karl Zipser <karlzipser@berkeley.edu>
2016-02-06 23:37:25 -0800

    gacp

--- RPi3/capture_client.py
+++ RPi3/capture_client.py
@@ -14,8 +14,8 @@ connection = client_socket.makefile('wb')
 try:
     with picamera.PiCamera() as camera:
         print 'warming up'
-        camera.resolution = (150,112)#(300,225)#(640, 480)#(640, 480)# #(300,225)
-        camera.framerate = 20
+        camera.resolution = (128,96)#(150,112)#(300,225)#(640, 480)#(640, 480)# #(300,225)
+        camera.framerate = 15
         # Start a preview and let the camera warm up for 2 seconds
         #camera.start_preview()
         time.sleep(2)
