d2b3d1d97f1c43fe34c476e26c232dfecc226847
Karl Zipser <karlzipser@berkeley.edu>
2016-02-07 10:51:12 -0800

    gacp

--- RPi3/RPi_server.py
+++ RPi3/RPi_server.py
@@ -141,7 +141,7 @@ def update_driving(buf):
         print "cruise on!!!!"
         cruise_control = True
         cruise_control_on_t = time.time()
-        cruise_rps = 2#rps
+        cruise_rps = 3#rps
         cruise_speed = speed
     if cruise_control:
         if time.time() - cruise_control_on_t > 1:
