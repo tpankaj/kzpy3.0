26d8a0bf88e19548f4a2bf6fb8899689a4aef826
Karl Zipser <karlzipser@hiMac.local>
2016-02-11 13:02:24 -0800

    gacp

--- RPi3/view_drive_data__.py
+++ RPi3/view_drive_data__.py
@@ -3,6 +3,52 @@ from kzpy3.vis import *
 fig = plt.figure(figsize=(20,3))
 #ax = fig.add_axes([0, 0, 1, 1], frameon=False)
 
+# 0_1455048664.8_str=0_spd=0_rps=0_lrn=36_rrn=100_rnd=0_
+
+
+def get_run_data(run_path):
+    _,l=dir_as_dic_and_list(run_path)
+    index = []
+    timestamp = []
+    steer = []
+    speed = []
+    rps = []
+    left_range = []
+    right_range = []
+    rand_control = []
+    for m in l:
+        n = m.split('_')
+        p = n[0]
+        index.append(int(p))
+        p = n[1]
+        timestamp.append(float(p))
+        p = n[2].split('=')[1]
+        steer.append(float(p)/100.0)
+        p = n[3].split('=')[1]
+        speed.append(float(p)/100.0)
+        p = n[4].split('=')[1]
+        rps.append(float(p)/10.0)
+        p = n[5].split('=')[1]
+        left_range.append(int(p))
+        p = n[6].split('=')[1]
+        right_range.append(int(p))
+        p = n[7].split('=')[1]
+        rand_control.append(int(p))
+    run_data_dic = {}
+    run_data_dic['run_path'] = run_path
+    run_data_dic['img_lst'] = l
+    run_data_dic['index'] = index
+    run_data_dic['timestamp'] = timestamp
+    run_data_dic['steer'] = steer
+    run_data_dic['speed'] = speed
+    run_data_dic['rps'] = rps
+    run_data_dic['left_range'] = left_range
+    run_data_dic['right_range'] = right_range
+    run_data_dic['rand_control'] = rand_control
+    return run_data_dic
+
+
+
 data_dir = '/Users/karlzipser/Desktop/RPi3_data/10Feb16_13h12m56s'
 
 _,l=dir_as_dic_and_list(data_dir)
