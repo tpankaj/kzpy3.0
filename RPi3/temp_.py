babac2175ef5bb147001a397bf1687aa396f57ad
Karl Zipser <karlzipser@hiMac.local>
2016-02-12 09:52:36 -0800

    gacp

--- RPi3/temp_.py
+++ RPi3/temp_.py
@@ -3,6 +3,7 @@ from kzpy3.vis import *
 
 #  rdd = get_run_data('/Users/karlzipser/Desktop/RPi3_data/10Feb16_13h31m38s')
 
+fig = plt.figure(figsize=(15,3))
 #ax = fig.add_axes([0, 0, 1, 1], frameon=False)
 
 # 0_1455048664.8_str=0_spd=0_rps=0_lrn=36_rrn=100_rnd=0_
@@ -56,8 +57,8 @@ def get_all_runs_dic(RPi3_data_path):
     return all_runs_dic
 
 def plot_run(run_data_dic):
-    #plt.ion()
-    #plt.clf()
+    plt.ion()
+    plt.clf()
     ts = run_data_dic['timestamp']
     fps = len(ts)/ts[-1]
     indx = 0
@@ -68,13 +69,13 @@ def plot_run(run_data_dic):
         ctr += 1
     plt.xlim([0,len(ts)])
     plot_run_timestamp_deltas(run_data_dic,max_thresh=3)
-    plt.plot(run_data_dic['steer'],'b',label='steer')
-    plt.plot(2.5+run_data_dic['rand_control']/4.0,'r',label='rand_control')
-    plt.plot(run_data_dic['speed'],'k',label='speed')
+    plt.plot(run_data_dic['steer'],'k',label='steer')
+    plt.plot(run_data_dic['rand_control']/4.0,'r',label='rand_control')
+    plt.plot(run_data_dic['speed'],'b',label='speed')
     plt.plot(run_data_dic['rps']/3.0,'g',label='rps/3')
     plt.title(run_data_dic['run_path'].split('/')[-1])
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    #plt.show()
+    plt.show()
 
 def test_run_timing(run_data_dic):
     index_errors = 0
@@ -95,41 +96,12 @@ def plot_run_timestamp_deltas(run_data_dic,max_thresh=1):
         d = run_data_dic['timestamp'][i]-run_data_dic['timestamp'][i-1]
         d = min(max_thresh,d)
         deltas.append(d)
-    plot(deltas,'ro')
+    plot(deltas,'o')
     return deltas
 
-current_key = ''
-all_runs_dic = {}
-some_data = {}
 
 def button_press_event(event):
-    current_key = some_data['current_key']
-    all_runs_dic = some_data['all_runs_dic']
-    play_range = some_data['play_range']
     for x in range(np.int(np.floor(event.xdata))+play_range[0],np.int(np.floor(event.xdata))+play_range[1]):
-        run_data_dic = all_runs_dic[current_key]
-        f = run_data_dic['img_lst'][x]
-        r = run_data_dic['rand_control'][x]
-        #print f
-        img = imread(opj(run_data_dic['run_path'],f))
-        if r > 0:
-            img[:,:,1:] *= 0.5
-        #plt.figure(1).add_subplot(2,3,5)
-        #plt.cla()
-        figure(2)
-        plt.clf()
-        mi(img,2)#1,[2,3,5])
-        plt.pause(0.0001)
-
-
-
-def show_frames(start,stop,all_runs_dic,current_key):
-    #play_range = (0,9)
-
-    plt.figure(1)
-
-    plt.plot([start,stop],[2.8,2.8])
-    for x in range(start,stop):
         run_data_dic = all_runs_dic[current_key]
         f = run_data_dic['img_lst'][x]
         r = run_data_dic['rand_control'][x]
@@ -143,22 +115,7 @@ def show_frames(start,stop,all_runs_dic,current_key):
         mi(img,10,img_title=d2s(x))
         plt.pause(0.01)
     plt.figure(1)
-
-
-all_runs_dic = get_all_runs_dic(opjD('RPi3_data/runs'))
-k = sorted(all_runs_dic.keys())
-  
-play_range = (0,9)
-plt.clf()
-fig = plt.figure(1,figsize=(7,2))
-plt.ion()
-plt.show()
-fig.canvas.mpl_connect('button_press_event', button_press_event)
-current_key=k[-4];plot_run(all_runs_dic[current_key])
-some_data['current_key'] = current_key
-some_data['all_runs_dic'] = all_runs_dic
-some_data['play_range'] = play_range
-
+    
 """
 5884
 5888
@@ -185,11 +142,12 @@ some_data['play_range'] = play_range
 6605
 6626
 
-fig = plt.figure(figsize=(15,3))
+play_range = (0,5*30)
+fig.canvas.mpl_connect('button_press_event', button_press_event)
 
-
-
-
+all_runs_dic = get_all_runs_dic(opjD('RPi3_data/runs'))
+current_key=k[-1];plot_run(all_runs_dic[current_key])
+k = sorted(all_runs_dic.keys())
 current_key = k[0]
 plot_run(all_runs_dic[k[0]])
 
@@ -199,14 +157,6 @@ for r in k:
         test_run_timing(all_runs_dic[r])
     except Exception,e:
         print e
-
-
-
-
-
-
-plt.hist(np.random.randn(1000))
-
 """
-
+)
 
