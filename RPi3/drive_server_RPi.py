98b738f90f4c344c55aaf0ca049d74c5a17a1786
Karl Zipser <karlzipser@berkeley.edu>
2016-02-14 17:06:24 -0800

    gacp

--- RPi3/drive_server_RPi.py
+++ RPi3/drive_server_RPi.py
@@ -281,7 +281,6 @@ try:
             buf = buf.strip('?')
             okay = True
         except Exception, e:
-            print('\a')
             print(d2s(os.path.basename(sys.argv[0]),':',e,' ######### pwm_motor.ChangeDutyCycle(0)'))
             #buf = '0 0 0 PROBLEM'
             #print(d2s(bcolors.FAIL,'############################# Setting buf to',buf,bcolors.ENDC))
