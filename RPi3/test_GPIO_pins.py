f6739c58924abccb8c6b2cfcc5c7c34b4b17b392
Karl Zipser <karlzipser@berkeley.edu>
2016-02-23 00:11:37 -0800

    gacp

--- RPi3/test_GPIO_pins.py
+++ RPi3/test_GPIO_pins.py
@@ -2,11 +2,13 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
+# see http://pinout.xyz/pinout/pin7_gpio4 for guide to pins
 # tested 7 13 15 19 21 23 29 [31,33,35 not tested ] 37
 # pin 11 dead.
 
 def test_pin(p):	
 	GPIO.setup(p,GPIO.OUT)
+	ready = raw_input('ready?')
 	for i in range(5):
 		GPIO.output(p, True)
 		time.sleep(1/40.0)		
