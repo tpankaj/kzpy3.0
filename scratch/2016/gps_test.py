999dda33ba4352883eab2e9dc3b12e16d17b8d4e
Karl Zipser <karlzipser@berkeley.edu>
2016-02-26 08:14:36 -0800

    gacp

--- scratch/2016/gps_test.py
+++ scratch/2016/gps_test.py
@@ -1,6 +1,7 @@
 import gps
 # Listen on port 2947 (gpsd) of localhost
-session = gps.gps("localhost", "2947") session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 while True:
 	try:
 		report = session.next()
