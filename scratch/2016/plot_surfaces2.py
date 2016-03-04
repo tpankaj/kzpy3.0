05011b5889f243561179032fc0894cf0468c71de
Karl Zipser <karlzipser@hiMac.local>
2016-03-03 22:01:30 -0800

    gacp

--- scratch/2016/plot_surfaces2.py
+++ scratch/2016/plot_surfaces2.py
@@ -1,6 +1,45 @@
 import cortex
 import nibabel as nib
 from kzpy3.vis import *
+import math
+
+def angle_wrt_x(A,B):
+    """Return the angle between B-A and the positive x-axis.
+    Values go from 0 to pi in the upper half-plane, and from 
+    0 to -pi in the lower half-plane.
+    http://stackoverflow.com/questions/13543977/python-does-a-module-exist-which-already-find-an-angle-and-the-distance-between
+    """
+    ax, ay = A
+    bx, by = B
+    return math.atan2(by-ay, bx-ax)
+
+def dist(A,B):
+    ax, ay = A
+    bx, by = B
+    return math.hypot(bx-ax, by-ay)
+
+def hor_ver(ori):
+	if ori >= 0 and ori <= 90:
+		return ori
+	elif ori > 90 and ori <= 180:
+		return 180-ori
+	elif ori < 0 and ori >= -90:
+		return -ori
+	else:
+		return 180+ori
+
+test_img = zeros((31,31))
+A = (15,15)
+for x in range(31):
+	for y in range(31):
+		B = (x,y)
+		test_img[x,y] = hor_ver(np.degrees(angle_wrt_x(A, B)))
+mi(test_img,'test_img')
+
+
+
+
+
 
 db_path = '/anaconda/share/pycortex/db'
 
@@ -20,6 +59,23 @@ data = zeros((106,106,60))+0.5
 for i in range(shape(selected_voxel_xyzs)[1]):
 	x,y,z = selected_voxel_xyzs[:,i]
 	data[x,y,z] = 1
+rfx = h['voxel_rf_peaks'][1]
+rfy = h['voxel_rf_peaks'][0]
+rf_ori = 0*rfx
+rf_ecc = 0*rfx
+A = (64,64)
+for i in range(len(rfx)):
+	B = (rfx[i],rfy[i])
+	theta = angle_wrt_x(A, B)
+	d = dist(A, B)
+	rf_ori[i] = math.degrees(theta)
+	rf_ecc[i] = d
+
+data = zeros((106,106,60)) * np.nan
+for i in range(shape(selected_voxel_xyzs)[1]):
+	x,y,z = selected_voxel_xyzs[:,i]
+	data[x,y,z] = rf_ecc[i] #hor_ver(rf_ori[i])
+#data = zeroToOneRange(data)
 #
 #################################
 
@@ -41,16 +97,37 @@ def get_surfaces(subject,xfm):
 			surfaces['lh'][t][:,i] += xfm[i,3]
 			surfaces['rh'][t][:,i] += xfm[i,3]
 	for h in ['lh','rh']:
-		surfaces[h]['mid'] = (0.5*surfaces[h]['wm']+0.5*surfaces[h]['pia'])
+		surfaces[h]['mid0'] = (0.75*surfaces[h]['wm']+0.25*surfaces[h]['pia'])
+		surfaces[h]['mid1'] = (0.5*surfaces[h]['wm']+0.5*surfaces[h]['pia'])
+		surfaces[h]['mid2'] = (0.25*surfaces[h]['wm']+0.75*surfaces[h]['pia'])
 	l,r = cortex.db.get_surf(subject, 'flat', merge=False)
 	surfaces['lh']['flat'] = l[0]
 	surfaces['rh']['flat'] = r[0]
 	return surfaces
 
-Z = 25
-def display_slice(reference,surfaces,Z,data,img_title=Z):
+def get_pixel_voxel_mappings(surfaces):
+	pix_dics = {}
+	for h in ['lh','rh']:
+		f = surfaces[h]['flat'].copy()
+		f -= f.min()
+		pix_dics[h] = {}
+		bad_center = int(f.max()/2.0) # Don't know why, but the center is a problem.
+		for i in range(len(f)):
+			for l in ['wm','mid2','mid1','mid0','pia']:
+				x,y = int(f[i,0]),int(f[i,1])
+				if x == bad_center and y == bad_center:
+					pass
+				else:
+					X,Y,Z = int(surfaces[h][l][i,0]),int(surfaces[h][l][i,1]),int(surfaces[h][l][i,2])
+					if (x,y) not in pix_dics[h]:
+						pix_dics[h][(x,y)] = []
+					pix_dics[h][(x,y)].append((X,Y,Z))
+	return pix_dics
+
+def display_slice(reference,surfaces,Z,data,img_title=None):
+	if not img_title:
+		img_title = Z
 	mi(data[:,:,Z]+reference[:,:,Z]/reference.max(),do_clf=True,toolBar=True,do_axis=True,figure_num = img_title)
-	#pts = np.concatenate((surfaces['lh']['pia'],surfaces['rh']['pia'],surfaces['lh']['mid'],surfaces['rh']['mid'],surfaces['lh']['wm'],surfaces['rh']['wm']),axis=0)
 	pts = np.concatenate((surfaces['lh']['pia'],surfaces['rh']['pia'],surfaces['lh']['wm'],surfaces['rh']['wm']),axis=0)
 	pts_lst = []
 	for p in range(shape(pts)[0]):
@@ -69,19 +146,50 @@ surfaces = get_surfaces(subject,xfm)
 
 display_slice(reference,surfaces,25,data)
 
+pix_dics = get_pixel_voxel_mappings(surfaces)
 
+def get_flat_images(surfaces,pix_dics,data):
+	flat_imgs = {}
+	for h in ['lh','rh']:
+		f = surfaces[h]['flat'].copy()
+		f -= f.min()
+		flat_imgs[h] = np.zeros((f.max()+1,f.max()+1))
+		for xy in pix_dics[h]:
+			x,y = xy
+			vox_lst = pix_dics[h][xy]
+			ctr = 0.0
+			accum = 0.0
+			for XYZ in vox_lst:
+				X,Y,Z = XYZ
+				if not np.isnan(data[X,Y,Z]):
+					accum += data[X,Y,Z]
+					ctr +=1.0
+			if ctr > 0:
+				flat_imgs[h][xy[0],xy[1]] = accum / ctr
+	return flat_imgs
 
-pix_dics = {}
-flat_imgs = {}
+flat_imgs = get_flat_images(surfaces,pix_dics,data)
 
 for h in ['lh','rh']:
-	f =surfaces[h]['flat'].copy()
+	mi(flat_imgs[h],h+' ecc data',do_clf=True,toolBar=True,do_axis=True)
+
+
+
+
+
+
+"""
+# bkp
+flat_imgs = {}
+pix_dics = {}
+for h in ['lh','rh']:
+	f = surfaces[h]['flat'].copy()
 	f -= f.min()
 	flat_imgs[h] = np.zeros((f.max()+1,f.max()+1))
 	pix_dics[h] = {}
-	bad_center = int(f.max()/2.0)
+	bad_center = int(f.max()/2.0) # Don't know why, but the center is a problem.
 	for i in range(len(f)):
-		for l in ['wm','mid','pia']:
+		for l in ['wm','mid2','mid1','mid0','pia']:
 			x,y = int(f[i,0]),int(f[i,1])
 			if x == bad_center and y == bad_center:
 				pass
@@ -90,10 +198,13 @@ for h in ['lh','rh']:
 				if (x,y) not in pix_dics[h]:
 					pix_dics[h][(x,y)] = []
 				pix_dics[h][(x,y)].append((X,Y,Z))
-				flat_imgs[h][x,y] = max(data[X,Y,Z],flat_imgs[h][x,y])
+				flat_imgs[h][x,y] = data[X,Y,Z]#max(data[X,Y,Z],flat_imgs[h][x,y])
 	mi(flat_imgs[h],h+' data',do_clf=True,toolBar=True,do_axis=True)
+"""
 
 
+
+"""
 roi_flatmasks = {}
 roi_masks = {}
 for h in ['lh','rh']:
@@ -112,7 +223,7 @@ for h in ['lh','rh']:
 		for xyz in XYZs:
 			roi_masks[h][xyz[0],xyz[1],xyz[2]] = 1
 	display_slice(reference,surfaces,25,roi_masks[h],'roi '+h)
-
+"""
 
 
 
@@ -140,6 +251,8 @@ display_slice(reference,surfaces,20,data,'data')
 
 
 """
+Commands to prepare flat surfaces:
+
 tksurfer S12015 lh smoothwm
 save patch as: lh.S12015_flat.patch.3d
 cd freesurfer/subjects/S12015/surf/
