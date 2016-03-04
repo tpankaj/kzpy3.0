45f8bd868663e81220c163b8b029caf0aba3e7f5
Karl Zipser <karlzipser@hiMac.local>
2016-03-04 10:45:00 -0800

    gacp

--- scratch/2016/plot_surfaces2.py
+++ scratch/2016/plot_surfaces2.py
@@ -3,86 +3,9 @@ import nibabel as nib
 from kzpy3.vis import *
 import math
 
-def angle_wrt_x(A,B):
-    """Return the angle between B-A and the positive x-axis.
-    Values go from 0 to pi in the upper half-plane, and from 
-    0 to -pi in the lower half-plane.
-    http://stackoverflow.com/questions/13543977/python-does-a-module-exist-which-already-find-an-angle-and-the-distance-between
-    """
-    ax, ay = A
-    bx, by = B
-    return math.atan2(by-ay, bx-ax)
-
-def dist(A,B):
-    ax, ay = A
-    bx, by = B
-    return math.hypot(bx-ax, by-ay)
-
-def hor_ver(ori):
-	if ori >= 0 and ori <= 90:
-		return ori
-	elif ori > 90 and ori <= 180:
-		return 180-ori
-	elif ori < 0 and ori >= -90:
-		return -ori
-	else:
-		return 180+ori
-
-test_img = zeros((31,31))
-A = (15,15)
-for x in range(31):
-	for y in range(31):
-		B = (x,y)
-		test_img[x,y] = hor_ver(np.degrees(angle_wrt_x(A, B)))
-mi(test_img,'test_img')
-
-
-
-
-
-
-db_path = '/anaconda/share/pycortex/db'
-
-########## specific data #########
+# https://education.github.com/pack
+################################################
 #
-subject = 'S12015'
-transform_name = '9Feb2015'
-reference = (nib.load(opj(db_path,subject,'transforms',transform_name,'reference.nii.gz'))).get_data()
-xfm = np.load(opj(db_path,subject,'transforms',transform_name,'coord.npy'))
-
-import h5py
-f = h5py.File('/Users/karlzipser/2015/10/2015-10/HVO_RF_mapping.736252.5459.mat','r')
-h = f['HVO_RF_mapping']
-selected_voxel_xyzs = h['selected_voxel_xyzs'][:,:]
-selected_voxel_xyzs -= 1 # matlab to python indexing
-data = zeros((106,106,60))+0.5
-for i in range(shape(selected_voxel_xyzs)[1]):
-	x,y,z = selected_voxel_xyzs[:,i]
-	data[x,y,z] = 1
-rfx = h['voxel_rf_peaks'][1]
-rfy = h['voxel_rf_peaks'][0]
-rf_ori = 0*rfx
-rf_ecc = 0*rfx
-A = (64,64)
-for i in range(len(rfx)):
-	B = (rfx[i],rfy[i])
-	theta = angle_wrt_x(A, B)
-	d = dist(A, B)
-	rf_ori[i] = math.degrees(theta)
-	rf_ecc[i] = d
-
-ori_data = zeros((106,106,60)) * np.nan
-ecc_data = zeros((106,106,60)) * np.nan
-for i in range(shape(selected_voxel_xyzs)[1]):
-	x,y,z = selected_voxel_xyzs[:,i]
-	ori_data[x,y,z] = hor_ver(rf_ori[i])/90.0 #rf_ecc[i] #
-	ecc_data[x,y,z] = rf_ecc[i]/64.0
-#data = zeroToOneRange(data)
-#
-#################################
-
-
-
 def get_surfaces(subject,xfm):
 	"""
 	Use pycortex functionality to load white matter and pial surfaces.
@@ -133,7 +56,7 @@ def display_slice(reference,surfaces,Z,data,img_title=None):
 	pts = np.concatenate((surfaces['lh']['pia'],surfaces['rh']['pia'],surfaces['lh']['wm'],surfaces['rh']['wm']),axis=0)
 	pts_lst = []
 	for p in range(shape(pts)[0]):
-	    if pts[p,2] > Z-0.5 and pts[p,2] < Z+0.5:
+	    if pts[p,2] >= Z-0.5 and pts[p,2] < Z+0.5:
 	        pts_lst.append(pts[p,:])
 	pts_lst = np.array(pts_lst)
 	piax = pts_lst[:,0]
@@ -167,12 +90,103 @@ def get_flat_images(surfaces,pix_dics,data,colorizer=red_green):
 			else:
 				flat_imgs[h][xy[0],xy[1],:] = 0.5
 	return flat_imgs
+#
+###################################################
 
-####################
+####################### ori & ecc utilities ##################
+#
+def angle_wrt_x(A,B):
+    """Return the angle between B-A and the positive x-axis.
+    Values go from 0 to pi in the upper half-plane, and from 
+    0 to -pi in the lower half-plane.
+    http://stackoverflow.com/questions/13543977/python-does-a-module-exist-which-already-find-an-angle-and-the-distance-between
+    """
+    ax, ay = A
+    bx, by = B
+    return math.atan2(by-ay, bx-ax)
+
+def dist(A,B):
+    ax, ay = A
+    bx, by = B
+    return math.hypot(bx-ax, by-ay)
+
+def hor_ver(ori):
+	if ori >= 0 and ori <= 90:
+		return ori
+	elif ori > 90 and ori <= 180:
+		return 180-ori
+	elif ori < 0 and ori >= -90:
+		return -ori
+	else:
+		return 180+ori
+"""
+test_img = zeros((31,31))
+A = (15,15)
+for x in range(31):
+	for y in range(31):
+		B = (x,y)
+		test_img[x,y] = hor_ver(np.degrees(angle_wrt_x(A, B)))
+mi(test_img,'test_img')
+"""
+#
+#############################################
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+
+
+########## general data #########
+#
+db_path = '/anaconda/share/pycortex/db'
+subject = 'S12015'
+transform_name = '9Feb2015'
+reference = (nib.load(opj(db_path,subject,'transforms',transform_name,'reference.nii.gz'))).get_data()
+xfm = np.load(opj(db_path,subject,'transforms',transform_name,'coord.npy'))
+#
+##################################
+
+########## specific data #########
+#
+import h5py
+f = h5py.File('/Users/karlzipser/2015/10/2015-10/HVO_RF_mapping.736252.5459.mat','r')
+h = f['HVO_RF_mapping']
+selected_voxel_xyzs = h['selected_voxel_xyzs'][:,:]
+selected_voxel_xyzs -= 1 # matlab to python indexing
+data = zeros((106,106,60))+0.5
+for i in range(shape(selected_voxel_xyzs)[1]):
+	x,y,z = selected_voxel_xyzs[:,i]
+	data[x,y,z] = 1
+rfx = h['voxel_rf_peaks'][1]
+rfy = h['voxel_rf_peaks'][0]
+rf_ori = 0*rfx
+rf_ecc = 0*rfx
+A = (64,64)
+for i in range(len(rfx)):
+	B = (rfx[i],rfy[i])
+	theta = angle_wrt_x(A, B)
+	d = dist(A, B)
+	rf_ori[i] = math.degrees(theta)
+	rf_ecc[i] = d
+ori_data = zeros((106,106,60)) * np.nan
+ecc_data = zeros((106,106,60)) * np.nan
+for i in range(shape(selected_voxel_xyzs)[1]):
+	x,y,z = selected_voxel_xyzs[:,i]
+	ori_data[x,y,z] = hor_ver(rf_ori[i])/90.0 #rf_ecc[i] #
+	ecc_data[x,y,z] = rf_ecc[i]/64.0
+#
+#################################
+
+
 
 surfaces = get_surfaces(subject,xfm)
 
-#display_slice(reference,surfaces,25,data)
+data_no_nan = data.copy()
+data_no_nan[np.isnan(data_no_nan)] = 0
+for z in range(20,30):
+	display_slice(reference,surfaces,z,data_no_nan)
 
 pix_dics = get_pixel_voxel_mappings(surfaces)
 
@@ -187,33 +201,14 @@ for h in ['lh','rh']:
 
 
 
-
-
-
-"""
-# bkp
-flat_imgs = {}
-pix_dics = {}
-for h in ['lh','rh']:
-	f = surfaces[h]['flat'].copy()
-	f -= f.min()
-	flat_imgs[h] = np.zeros((f.max()+1,f.max()+1))
-	pix_dics[h] = {}
-	bad_center = int(f.max()/2.0) # Don't know why, but the center is a problem.
-	for i in range(len(f)):
-		for l in ['wm','mid2','mid1','mid0','pia']:
-			x,y = int(f[i,0]),int(f[i,1])
-			if x == bad_center and y == bad_center:
-				pass
-			else:
-				X,Y,Z = int(surfaces[h][l][i,0]),int(surfaces[h][l][i,1]),int(surfaces[h][l][i,2])
-				if (x,y) not in pix_dics[h]:
-					pix_dics[h][(x,y)] = []
-				pix_dics[h][(x,y)].append((X,Y,Z))
-				flat_imgs[h][x,y] = data[X,Y,Z]#max(data[X,Y,Z],flat_imgs[h][x,y])
-	mi(flat_imgs[h],h+' data',do_clf=True,toolBar=True,do_axis=True)
-"""
-
+def get_voxel_ROI_masks(binary_flatmask_lh,binary_flatmask_rh):
+	binary_flatmasks = {}
+	binary_flatmasks['lh'] = binary_flatmask_lh
+	binary_flatmasks['rh'] = binary_flatmask_rh
+	for h in ['lh','rh']:
+		nonzeros = np.where(binary_flatmasks[h] > 0)
+		for x,y in zip(nonzeros[0],nonzeros[1]):
+			XYZs = pix_
 
 
 """
@@ -242,20 +237,16 @@ for h in ['lh','rh']:
 
 
 """
-K =  0.035
 surfaces = get_surfaces(subject,xfm)
-b = surfaces['lh']['pia'].copy()
-for i in range(len(b[:,0])):
-	if b[i,1] < 20:
-		b[i,1] += K*(b[i,1]-65)
-surfaces['lh']['pia'] = b
-c = surfaces['lh']['wm'].copy()
-for i in range(len(b[:,0])):
-	if c[i,1] < 20:
-		c[i,1] += K*(c[i,1]-65)
-surfaces['lh']['wm'] = c
-display_slice(reference,surfaces,20,data,'data')
-
+K =  0.25
+X = 20
+for h in ['lh','rh']:
+	for l in ['wm','mid2','mid1','mid0','pia']:
+		s = surfaces[h][l]
+		for i in range(len(s[:,0])):
+			if s[i,2] >= 28.5 and s[i,2] < 29.5:
+				if s[i,1] < X:
+					s[i,1] += K*(s[i,1]-X)
 
 """
 
