12f082a23986b7bf45bb4a1d94f246487a50f163
Karl Zipser <karlzipser@berkeley.edu>
2016-03-07 10:59:50 -0800

    gacp

--- caf/kaffe/y2016/m3/kaffe_new.py
+++ caf/kaffe/y2016/m3/kaffe_new.py
@@ -9,27 +9,63 @@ def mi_diffs(solver):
 		if len(l[1]) > 2:
 			mi(np.abs(solver.net.blobs[l[0]].diff[0,:,:,:]).mean(axis=0),l[0]+' diff')
 
+def show_py_image_data(data):
+	d = data[0].copy()
+	d = z2o(d)
+	d[:,0,0] = 1
+	d[:,0,1] = 0
+	for j in range(1):
+	    for i in range(9):
+
+	        mi(d[i,:,:])
+	        plt.pause(0.1)
+
 
 solver = caffe.SGDSolver(opjh("kzpy3/caf/training/y2016/m3/RPi3/solver_kaffe_11px.prototxt"))
+f=opjD('train_val_kaffe_11px_iter_500000.caffemodel')
+solver.net.copy_from(f)
+
+
+for i in range(9):
+	solver.net.blobs['ddata'].data[0][i,:,:] = np.random.random(np.shape(solver.net.blobs['ddata'].data[0][1,:,:]))-0.5
+
+
+step_size = 0.333
+jitter = 1
+
+def steer_show_gradient(solver,s):
+	for i in range(9):
+		solver.net.blobs['ddata'].data[0][i,:,:] = np.random.random(np.shape(solver.net.blobs['ddata'].data[0][1,:,:]))-0.5
+
+	src = solver.net.blobs['py_image_data']
+	solver.net.forward()
+	solver.net.blobs['ip2'].diff[0] *= 0
+	solver.net.blobs['ip2'].diff[0,s]=1
+	solver.net.backward(start='ip2')
+	show_py_image_data(solver.net)
 
 
 
 for i in range(9):
 	solver.net.blobs['ddata'].data[0][i,:,:] = np.random.random(np.shape(solver.net.blobs['ddata'].data[0][1,:,:]))-0.5
 
-step_size = 0.333
+step_size = 1.5#0.333
 jitter = 1
 src = solver.net.blobs['py_image_data']
-for j in range(10):
+for j in range(1):
 
 	ox, oy = np.random.randint(-jitter, jitter+1, 2)
 	src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2) # apply jitter shift
 
 	solver.net.forward()
+	solver.net.blobs['ip2'].diff[0] *= 0
+	solver.net.blobs['ip2'].diff[0,3]=1
+#	solver.net.blobs['conv1'].diff[0] *= 0
+#	solver.net.blobs['conv1'].diff[0,30,8,15] = 1
+#	solver.net.blobs['conv2'].diff[0,2,15,8] = 1
 
-	solver.net.blobs['ip2'].diff[0,0]=1
-
-	solver.net.backward()
+	solver.net.backward(start='ip2')
+#	solver.net.backward(start='conv1')
 
 	g = src.diff[0]
 	
@@ -37,9 +73,5 @@ for j in range(10):
 
 	src.data[0] = np.roll(np.roll(src.data[0], -ox, -1), -oy, -2) # unshift image
 
-	mi_diffs(solver)
-	mi(src.data[0,0,:,:],'py_image_data')
-	mi(g[0,:,:],'g')
-	plt.pause(1)
 
 
