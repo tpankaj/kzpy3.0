6d9b95807fd6289dee45f570b5838800220fc8f4
Karl Zipser <karlzipser@hiMac.local>
2016-03-09 10:32:40 -0800

    gacp

--- caf/kaffe/y2016/m3/kaffe_for_train_val_11px_force_pad.py
+++ caf/kaffe/y2016/m3/kaffe_for_train_val_11px_force_pad.py
@@ -17,7 +17,7 @@ def show_py_image_data(data,fig=1):
 #solver = caffe.SGDSolver(opjh("kzpy3/caf/training/y2016/m3/RPi3/solver_kaffe_11px.prototxt"))
 #f=opjD('train_val_kaffe_11px_iter_2600000.caffemodel')
 solver = caffe.SGDSolver(opjh("kzpy3/caf/training/y2016/m3/RPi3/solver_kaffe_11px_RGB.prototxt"))
-f=opjD('train_val_kaffe_11px_RGB_iter_600000.caffemodel')
+f=opjD('train_val_kaffe_11px_RGB_iter_2500000.caffemodel')
 solver.net.copy_from(f)
 
 
@@ -114,6 +114,7 @@ for i in range(n):
 	if solver.net.blobs['ip2'].data[0].argmax(axis=0) == solver.net.blobs['py_target_data'].data[0].argmax(axis=0):
 		n_correct += 1
 print(d2s('percent correct =',n_correct,'/',n,'chance =',int(1/7.0*n),'/',n))
+print(d2s('percent correct =',n_correct/(1.0*n))
 
 def img_from_caffe_data(data):
 	img = np.zeros((shape(data)[2],shape(data)[3],3))
@@ -128,41 +129,45 @@ def img_from_caffe_data(data):
 
 
 
-
-
-
-
-for i in range(3):
-	solver.net.blobs['C_ddata'].data[0][i,:,:] = np.random.random(np.shape(solver.net.blobs['C_ddata'].data[0][1,:,:]))-0.5
-#solver.net.blobs['py_image_data'].data[0] = 0*solver.net.blobs['py_image_data'].data[0]
-#solver.net.blobs['ddata'].data[0] = 0*solver.net.blobs['ddata'].data[0]
+results = {}
+ctr = 0
 
 img = imread('/Users/karlzipser/Desktop/RPi3_data/runs_scl_100_RGB/09Feb16_14h03m20s_scl=100_mir=0/3091_1455055614.01_str=0_spd=48_rps=28_lrn=0_rrn=0_rnd=0_scl=100_mir=0_.jpg')
-solver.net.blobs['C_ddata'].data[0,0,:,:] = img[:,:,0]
-solver.net.blobs['C_ddata'].data[0,1,:,:] = img[:,:,1]
-solver.net.blobs['C_ddata'].data[0,2,:,:] = img[:,:,2]
 
-jitter = 3
-for j in range(50):
-	ox, oy = np.random.randint(-jitter, jitter+1, 2)
-	solver.net.blobs['C_ddata'].data[0] = np.roll(np.roll(solver.net.blobs['C_ddata'].data[0], ox, -1), oy, -2) # apply jitter shift
-	
-	solver.net.forward()#start='ddata')
-	p = solver.net.blobs['C_ip2'].data[0,:]; p = (p*100).astype(int)/100.0; print p
-#	print  solver.net.blobs['ip2'].data[:]
-	solver.net.blobs['C_ip2'].diff[0] *= 0
-	solver.net.blobs['C_ip2'].diff[0,3]=1
-#	solver.net.blobs['conv1'].diff[0] *= 0
-#	solver.net.blobs['conv1'].diff[0,30,8,15] = 1
-#	solver.net.blobs['conv2'].diff[0,2,15,15] = 1
-	solver.net.backward(start='C_ip2')
-#	solver.net.backward(start='conv2')
-	g = solver.net.blobs['C_ddata'].diff[0]
-	solver.net.blobs['C_ddata'].data[:] += 0.005/np.abs(g).mean() * g
-	
-	solver.net.blobs['C_ddata'].data[:] = z2o(solver.net.blobs['C_ddata'].data[:]) - 0.5
-	solver.net.blobs['C_ddata'].data[:] *= 0.95
-	#solver.net.blobs['ddata'].data[0][i,:,:] += 0.001*(np.random.random(np.shape(solver.net.blobs['ddata'].data[0][1,:,:]))-0.5)
-	solver.net.blobs['C_ddata'].data[0] = np.roll(np.roll(solver.net.blobs['C_ddata'].data[0], -ox, -1), -oy, -2) # unshift image
+for k in [0,3,6]:#range(7):
+	#for i in range(3):
+	#	solver.net.blobs['C_ddata'].data[0][i,:,:] = np.random.random(np.shape(solver.net.blobs['C_ddata'].data[0][1,:,:]))-0.5
+	#solver.net.blobs['py_image_data'].data[0] = 0*solver.net.blobs['py_image_data'].data[0]
+	#solver.net.blobs['ddata'].data[0] = 0*solver.net.blobs['ddata'].data[0]
 
+	
+	solver.net.blobs['C_ddata'].data[0,0,:,:] = img[:,:,0].copy()
+	solver.net.blobs['C_ddata'].data[0,1,:,:] = img[:,:,1].copy()
+	solver.net.blobs['C_ddata'].data[0,2,:,:] = img[:,:,2].copy()
+	
+	jitter = 3
+	for j in range(75):
+		ox, oy = np.random.randint(-jitter, jitter+1, 2)
+		solver.net.blobs['C_ddata'].data[0] = np.roll(np.roll(solver.net.blobs['C_ddata'].data[0], ox, -1), oy, -2) # apply jitter shift
+		
+		solver.net.forward()#start='ddata')
+		p = solver.net.blobs['C_ip2'].data[0,:]; p = (p*100).astype(int)/100.0; print p
+	#	print  solver.net.blobs['ip2'].data[:]
+	#	solver.net.blobs['C_ip2'].diff[0] *= 0
+	#	solver.net.blobs['C_ip2'].diff[0,3]=1
+		solver.net.blobs['C_ip2'].diff[0] *= 0
+		solver.net.blobs['C_ip2'].diff[0,k] = 1
+		solver.net.backward(start='C_ip2')
+	#	solver.net.backward(start='conv2')
+		g = solver.net.blobs['C_ddata'].diff[0]
+		solver.net.blobs['C_ddata'].data[:] += 0.005/np.abs(g).mean() * g
+		
+		solver.net.blobs['C_ddata'].data[:] = z2o(solver.net.blobs['C_ddata'].data[:]) - 0.5
+		solver.net.blobs['C_ddata'].data[:] *= 0.95
+		#solver.net.blobs['ddata'].data[0][i,:,:] += 0.001*(np.random.random(np.shape(solver.net.blobs['ddata'].data[0][1,:,:]))-0.5)
+		solver.net.blobs['C_ddata'].data[0] = np.roll(np.roll(solver.net.blobs['C_ddata'].data[0], -ox, -1), -oy, -2) # unshift image
+	results[ctr] = img_from_caffe_data(solver.net.blobs['C_ddata'].data)
+	mi(results[ctr],ctr,img_title=d2s(k))
+	ctr += 1
+	plt.pause(0.01)
 
