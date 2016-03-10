6a24f707dd7ba5bfd71c10605404626abe30339a
Karl Zipser <karlzipser@hiMac.local>
2016-03-09 16:15:32 -0800

    gacp

--- RPi3/view_drive_data2.py
+++ RPi3/view_drive_data2.py
@@ -12,9 +12,9 @@ CAFFE_DEPLOY_MODE = 'CAFFE_DEPLOY_MODE'
 USE_GRAPHICS = 'USE_GRAPHICS'
 
 CAFFE_DATA = opjh('Desktop/RPi3_data/runs_scale_50_BW')
-#CAFFE_FRAME_RANGE = (-15,-6) # (-7,-6)# 
+CAFFE_FRAME_RANGE = (-15,-6) # (-7,-6)# 
 #CAFFE_DATA = opjh('Desktop/RPi3_data/runs_scl_100_RGB')
-CAFFE_FRAME_RANGE = (-7,-6)# 
+#CAFFE_FRAME_RANGE = (-7,-6)# 
 
 run_mode = MC_CAFFE_TRAINING_MODE
 
