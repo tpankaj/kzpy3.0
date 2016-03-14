67fdb0ca256a7e032efc9120a8923d42025cded3
Karl Zipser <karlzipser@hiMac.local>
2016-03-13 23:23:04 -0700

    gacp

--- caf/kaffe/y2016/m3/temp.py
+++ caf/kaffe/y2016/m3/temp.py
@@ -1,13 +1,14 @@
 from kzpy3.utils import *
-n=100
-results = np.zeros((7,7))
-ctrs = np.zeros(7)
-for i in range(n):
-	solver.net.forward()
-	j = solver.net.blobs['py_target_data'].data[0].argmax(axis=0)
-	results[j,:] += solver.net.blobs['ip3'].data[0]
-	ctrs[j] += 1.0
-for i in range(7):
-	results[i,:] /= ctrs[i]
+def temp(solver):
+	n=100
+	results = np.zeros((7,7))
+	ctrs = np.zeros(7)
+	for i in range(n):
+		solver.net.forward()
+		j = solver.net.blobs['py_target_data'].data[0].argmax(axis=0)
+		results[j,:] += solver.net.blobs['ip3'].data[0]
+		ctrs[j] += 1.0
+	for i in range(7):
+		results[i,:] /= ctrs[i]
 
-print results
+	print results
