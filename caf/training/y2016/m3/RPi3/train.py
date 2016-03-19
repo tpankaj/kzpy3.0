f2ddf7f30af1f7a79678472e2283c149a1d39e5a
Karl Zipser <karlzipser@hiMac.local>
2016-03-19 11:17:23 -0700

    gacp

--- caf/training/y2016/m3/RPi3/train.py
+++ caf/training/y2016/m3/RPi3/train.py
@@ -21,8 +21,8 @@ from kzpy3.vis import *
 
 os.chdir(home_path) # this is for the sake of the train_val.prototxt
 
-training_path = opjh('kzpy3/caf/training/y2016/m2/from_mnist/original_with_accuracy')
-#training_path = opjh('kzpy3/caf/training/y2016/m3/RPi3')
+#training_path = opjh('kzpy3/caf/training/y2016/m2/from_mnist/original_with_accuracy')
+training_path = opjh('kzpy3/caf/training/y2016/m3/RPi3/scl25')
 #solver_name = 'solver_11px_scl50.prototxt'
 #solver_name = 'solver_11px_scl100_RGB.prototxt'
 #solver_name = 'solver_kaffe_11px.prototxt'
