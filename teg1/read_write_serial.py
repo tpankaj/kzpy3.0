a60a7b4f92dcff0c8357dfff97eda52d017c62f0
Karl Zipser <karlzipser@hiMac.local>
2016-04-13 14:01:01 -0700

    gacp

--- teg1/read_write_serial.py
+++ teg1/read_write_serial.py
@@ -24,11 +24,11 @@ motor_min_cpu = 1220
 
 cpu_int = -30000
 
-def pwm_to_percent(in_pwm,min_pwm,pwm_null,max_pwm):
+def pwm_to_percent(in_pwm,min_pwm,pwm_null,max_pwm,scale_factor=1.0):
     if in_pwm >= pwm_null:
-        out_percent = int((in_pwm - pwm_null)/(1.0*max_pwm - 1.0*pwm_null)*50 + 49)
+        out_percent = int(scale_factor*(in_pwm - pwm_null)/(1.0*max_pwm - 1.0*pwm_null)*50 + 49)
     else:
-        out_percent = int((in_pwm - pwm_null)/(1.0*pwm_null - 1.0*min_pwm)*49+49)
+        out_percent = int(scale_factor*(in_pwm - pwm_null)/(1.0*pwm_null - 1.0*min_pwm)*49+49)
     assert(out_percent >=0 and out_percent < 100)
     return out_percent
 
@@ -49,13 +49,18 @@ while True:
         in_steer_pwm = t[1]
         in_motor_pwm = t[0]
         in_button_pwm = t[2]
-        out_steer = pwm_to_percent(in_steer_pwm,servo_min_cpu,servo_null,servo_max_cpu)
-        out_motor = pwm_to_percent(in_motor_pwm,motor_min_cpu,motor_null,motor_max_cpu)
+        cpu_mode = -3
+        scale_factor = 1.0
         if abs(in_button_pwm - 1700) < 50:
             cpu_mode = 1 # human control
         elif abs(in_button_pwm - 1000) < 50:
             cpu_mode = 2 # cpu control
-        #print (out_steer,out_motor)
+        elif abs(in_button_pwm - 888) < 50:
+            cpu_mode = 2 # cpu control
+            scale_factor = 0.25
+        out_steer = pwm_to_percent(in_steer_pwm,servo_min_cpu,servo_null,servo_max_cpu)
+        out_motor = pwm_to_percent(in_motor_pwm,motor_min_cpu,motor_null,motor_max_cpu,scale_factor)
+       #print (out_steer,out_motor)
         cpu_int = encode_int_signal(cpu_mode,out_steer,out_motor)
     except Exception,e:
         print e
