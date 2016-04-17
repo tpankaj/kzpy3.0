aa656ca45e07b027926d38a863285c8970772c0b
Karl Zipser <karlzipser@hiMac.local>
2016-04-17 07:40:30 -0700

    gacp

--- teg1/camera.py
+++ teg1/camera.py
@@ -9,21 +9,15 @@ def camera_off():
 	kill_ps('gst-launch-0.10')
 
 def kill_ps(process_name_to_kill):
-
 	ax_ps_lst = unix('ps ax')
-
 	ps_lst = []
-
 	for p in ax_ps_lst:
 		if process_name_to_kill in p:
 			ps_lst.append(p)
-
 	pid_lst = []
 	for i in range(len(ps_lst)):
 		pid = int(ps_lst[i].split(' ')[1])
 		pid_lst.append(pid)
-
-	print pid_lst
-
+	#print pid_lst
 	for p in pid_lst:
 		unix(d2s('kill',p))
