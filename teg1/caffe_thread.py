5a9e61ab470ccb56475dec40467c1634304097c2
Karl Zipser <karlzipser@hiMac.local>
2016-04-16 23:00:30 -0700

    gacp

--- teg1/caffe_thread.py
+++ teg1/caffe_thread.py
@@ -1,13 +1,13 @@
 import caffe
 
-#caffe.set_device(0)
-caffe.set_mode_cpu()
-
 from kzpy3.vis import *
 
 os.chdir(home_path) # this is for the sake of the train_val.prototxt
 solver = caffe.SGDSolver(opjh("kzpy3/caf/training/y2016/m2/from_mnist/original_with_accuracy/solver_11px_no_py_layers.prototxt"))
 solver.net.copy_from(opjh('scratch/2016/2/16/caffe/models/from_mnist/original_with_accuracy_11px_iter_11100000.caffemodel'))
+
+caffe.set_mode_cpu()
+#caffe.set_device(0)
 
 for l in [(k, v.data.shape) for k, v in solver.net.blobs.items()]:
     print(l)
