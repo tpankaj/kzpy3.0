403419de443c5191d334098fade25ce5f1f9f9b7
Karl Zipser <karlzipser@berkeley.edu>
2016-04-23 22:59:02 -0700

    gacp

--- teg1/analysis/frames_readied_for_video.py
+++ teg1/analysis/frames_readied_for_video.py
@@ -1,15 +1,19 @@
 from kzpy3.utils import *
 
-def make_frame_aliases(input_dir,img_range=()):
+def frames_to_video_with_ffmpeg(input_dir,img_range=()):
 	_,fnames = dir_as_dic_and_list(input_dir)
+	frames_folder = input_dir.split('/')[-1]
 	if len(img_range) == 0:
 		img_range = (0,len(fnames))
-	temp_dir = opjD('temp'+'_'+input_dir.split('/')[-1])
+	temp_dir = opjD('temp'+'_'+frames_folder)
 	unix(d2s('mkdir -p',temp_dir))
 	ctr = 0
 	for i in range(img_range[0],img_range[1]):
 		unix(d2s('ln -s',opj(input_dir,fnames[i]),opj(temp_dir,d2n(ctr,'.jpg'))),False)
 		ctr+=1
+	#unix('ffmpeg -i '+temp_dir+'/%d.jpg -pix_fmt yuv420p -r 15 -b:v 14000k '+opjD(frames_folder)+'.mpg')
+	unix('ffmpeg -r 15 -i '+temp_dir+'/%d.jpg -vcodec mpeg4 -b 14000k '+opjD(frames_folder)+'.avi')
+
 
 """
 ffmpeg  -r 15 -i %d.jpg output.gif
---
+++ teg1/motor_servo_worker_2.py
@@ -0,0 +1,79 @@
+from kzpy3.utils import *
+from kzpy3.teg1.camera import *
+import serial
+
+# Two kinds of complications. OsX and ubuntu have different ways of 
+# specifying serial port. And, on ubuntu, the ACM1 can depend on both
+# the order of the USBs in the connectors and the order in which they
+# are plugged in. Thus, it would be good to have some error checking.
+if '/Users/' in home_path:
+    ser = serial.Serial('/dev/tty.usbmodem1461',9600) #115200)
+else:
+    ser = serial.Serial('/dev/ttyACM0',9600)
+
+# These state codes must match those in the motor_servo Arduino code.
+STATE_LOCK = 2
+STATE_LOCK_CALIBRATE = 4
+STATE_HUMAN_FULL_CONTROL = 1
+STATE_CAFFE_CAFFE_STEER_HUMAN_MOTOR = 3
+STATE_CAFFE_HUMAN_STEER_HUMAN_MOTOR = 5
+
+caffe_int = -30000
+camera_off_flag = True
+caffe_mode = 1 # no function for now
+
+
+
+def encode_int_signal(caffe_mode,caffe_steer,caffe_motor):
+    assert(caffe_mode >= -3 and caffe_mode <= 3 )
+    assert(caffe_steer >=0 and caffe_steer < 100)
+    assert(caffe_motor >=0 and caffe_motor < 100)
+    return 10000*caffe_mode + 100*caffe_steer + caffe_motor
+
+unix('mkdir -p ' + opjD('teg_data'))
+
+subprocess.Popen(['python',opjh('kzpy3/teg1/sensor_worker.py')])
+
+f = open(opjD('teg_data','_'+time_str()+'.motor_servo.txt'), 'w')
+
+while True:
+    try:
+        ser.write(d2s('(',caffe_int,')'))
+        s = ser.readline()
+        exec('t = ' + s)
+        assert(type(t) == tuple)
+        # do error checking on bounds
+        t = list(t)
+        t.append(time.time())
+        f.write(d2s(t,'\n'))
+        #print t
+        in_state = t[0]
+        in_steer = t[1]
+        in_motor = t[2]
+        in_state_change_time = t[3]
+
+        if in_state == STATE_HUMAN_FULL_CONTROL:
+            if camera_off_flag and in_state_change_time >= 500:
+                    camera_on(opjh('Desktop/teg_data',time_str()))
+                    camera_off_flag = False
+                    np.save(opjh('Desktop/caffe_quit_command.npy'),1)
+        elif in_state == STATE_LOCK or in_state == STATE_LOCK_CALIBRATE:
+            if camera_off_flag == False:
+                camera_off()
+                camera_off_flag = True
+                np.save(opjh('Desktop/caffe_quit_command.npy'),1)
+        elif in_state == STATE_CAFFE_CAFFE_STEER_HUMAN_MOTOR or in_state == STATE_CAFFE_HUMAN_STEER_HUMAN_MOTOR:
+            if camera_off_flag and in_state_change_time >= 500::
+                camera_on(opjh('Desktop/teg_data',time_str()))
+                camera_off_flag = False
+                np.save(opjh('Desktop/caffe_quit_command.npy'),0)
+                subprocess.Popen(['python',opjh('kzpy3/teg1/caffe_worker.py')])
+
+        caffe_steer = int(np.load(opjh('Desktop/caffe_command.npy')))
+        #print caffe_steer
+        out_steer = 49+caffe_steer
+        out_motor = in_motor
+        caffe_int = encode_int_signal(caffe_mode,out_steer,out_motor)
+    except Exception,e:
+        print(d2s(os.path.basename(sys.argv[0]),':',e))
+
