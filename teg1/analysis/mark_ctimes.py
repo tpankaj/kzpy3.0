621f4a74bd020545a79d1317212d6260b3c382bc
Karl Zipser <karlzipser@hiMac.local>
2016-05-02 09:12:14 -0700

    gacp

--- teg1/analysis/mark_ctimes.py
+++ teg1/analysis/mark_ctimes.py
@@ -9,7 +9,7 @@ def mark_ctimes():
 		for m in l:
 			c = os.path.getctime(opj(d,m))
 			clst.append(d2s(c,m))
-		list_of_strings_to_txt_file('ctimes_'+d,clst)
+		list_of_strings_to_txt_file('ctimes_'+d+'.txt',clst)
 
 if __name__ == '__main__':
 	mark_ctimes()
