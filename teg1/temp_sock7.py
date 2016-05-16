c1040830338758c02ef5aa9e523966e8b36b87d5
Karl Zipser <karlzipser@berkeley.edu>
2016-05-16 09:07:15 -0400

    gacp

--- teg1/temp_sock7.py
+++ teg1/temp_sock7.py
@@ -6,12 +6,14 @@ client_setup = False
 try:
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect(('localhost', 5000))
+    #clientsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     buf = '<'+time_str()+'>'
     while len(buf)<64:
         buf += '?'
     assert len(buf) == 64
     clientsocket.send(buf)
     client_setup = True
+
 except:
     pass
 
@@ -21,9 +23,11 @@ if client_setup:
     port = 8000
 else:
     port = 5000
+    subprocess.Popen(['python',opjh('kzpy3/teg1/temp_sock7.py')])#unix('python /Users/karlzipser/kzpy3/teg1/.py')
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
 serversocket.listen(5)
+#serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 TIMEOUT_DURATION = 0.0333
 print 'Waiting for connection . . .'
 connection, address = serversocket.accept()
@@ -58,10 +62,10 @@ try:
             buf = buf.strip('?')
             okay = True
         except Exception, e:
-            print(d2s(os.path.basename(sys.argv[0]),':',e,' \a ######### pwm_motor.ChangeDutyCycle(0)'))
+            pass #print(d2s(os.path.basename(sys.argv[0]),':',e,' \a ######### pwm_motor.ChangeDutyCycle(0)'))
         if okay:
             if len(buf) != "":
-                print buf
+                print str(port)+' '+buf
             else:
                 print("\a *** No Data received from socket ***")
                 cleanup_and_exit()
@@ -70,8 +74,9 @@ try:
         while len(buf)<64:
             buf += '?'
         assert len(buf) == 64
-        clientsocket.send(buf)
-        time.sleep(0.1)
+        if np.random.random() < 0.01:
+            clientsocket.send(buf)
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
     print(d2s(os.path.basename(sys.argv[0]),':','KeyboardInterrupt \a'))
