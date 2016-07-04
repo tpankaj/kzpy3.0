48294612634d1ee32f28a04ab40bfb722bd99729
Karl Zipser <karlzipser@berkeley.edu>
2016-07-04 15:31:23 -0700

    gacp

--- teg1/cam15Hz320x240.sh
+++ teg1/cam15Hz320x240.sh
@@ -2,4 +2,8 @@
 
 echo "Camera: 320x240, 15 Hz"
 
-gst-launch -e v4l2src ! video/x-raw-yuv,format=\(fourcc\)YUY2,width=320,height=240,framerate=15/1 ! ffmpegcolorspace ! videorate ! video/x-raw-rgb,framerate=15/1 ! ffmpegcolorspace ! jpegenc ! multifilesink location="frame%05d.jpeg" &
+## this is for the Jetson TK1
+# gst-launch -e v4l2src ! video/x-raw-yuv,format=\(fourcc\)YUY2,width=320,height=240,framerate=15/1 ! ffmpegcolorspace ! videorate ! video/x-raw-rgb,framerate=15/1 ! ffmpegcolorspace ! jpegenc ! multifilesink location="frame%05d.jpeg" &
+
+## this is for the Jetson TX1
+gst-launch-1.0 -e v4l2src ! video/x-raw,width=320,height=240! videorate ! video/x-raw,framerate=15/1 ! jpegenc ! multifilesink location="frame%05d.jpeg"
