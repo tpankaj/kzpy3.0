bcabbd2f439b2a84b6f89fc6c1b55ea9b6151b4f
Karl Zipser <karlzipser@berkeley.edu>
2016-08-06 07:15:46 -0700

    gacp

--- teg1/rosbag_work/learn_rosbag_analysis.py
+++ teg1/rosbag_work/learn_rosbag_analysis.py
@@ -22,17 +22,21 @@ for b in bag_files:
 
     bag = rosbag.Bag(b)
 
-    for m in bag.read_messages(topics=['/bair_car/steer']):
-        A['steer'][m[2].to_time()] = m[1].data
+    non_image_topics = ['steer','state','motor','gyro','gps','encoder','sonar']
+    for topic in non_image_topics:
+        for m in bag.read_messages(topics=['/bair_car/'+topic]):
+            A[topic][m.timestamp.to_time()] = m[1].data
 
-    for m in bag.read_messages(topics=['/bair_car/state']):
-        A['state'][m[2].to_time()] = m[1].data
+    #for m in bag.read_messages(topics=['/bair_car/state']):
+    #    A['state'][m[2].to_time()] = m[1].data
+
+
 
     for m in bag.read_messages(topics=['/bair_car/zed/left/image_rect_color']):
-        A['left_image'][m.timestamp.to_time()] = imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
+        A['left_image'][m.timestamp.to_time()] = 1#imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
 
     for m in bag.read_messages(topics=['/bair_car/zed/right/image_rect_color']):
-        A['right_image'][m.timestamp.to_time()] = imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
+        A['right_image'][m.timestamp.to_time()] = 1#imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
 
 
 def get_sorted_keys_and_data(dict):
