75aed15cd72d873a027f9ab3899d5b2598f01ea7
Karl Zipser <karlzipser@berkeley.edu>
2016-08-07 08:29:40 -0700

    gacp

--- teg1/rosbag_work/learn_rosbag_analysis.py
+++ teg1/rosbag_work/learn_rosbag_analysis.py
@@ -16,23 +16,27 @@ import cv_bridge
 from cv_bridge import CvBridge, CvBridgeError
 bridge = cv_bridge.CvBridge()
 
-############## bagfile data to dictionary A ##############################
+############## topics, not necessarily original rosbag names ###################
 #
-A = {}
-
 image_topics = ['left_image','right_image']
 single_value_topics = ['steer','state','motor','encoder','sonar']
 vector3_topics = ['gyro'] # leave out gps for now.
 all_topics = image_topics + single_value_topics + vector3_topics
-for topic in all_topics:
-    A[topic] = {}
+#
+######################################################################
 
+############## bagfile data to dictionary A ##############################
+#
+def do_A(bag_files_path,save_pngs=False,png_path='',scale_factor=1.0,apply_rectangles=False):
+    
+    A = {}
+    
+    for topic in all_topics:
+        A[topic] = {}
 
-def do_A(bag_files_path,save_pngs=False,png_path='',scale_factor=1.0):
-
-    bag_files = sorted(glob.glob(opj(bag_files_path,'*.bag')))  #'/home/karlzipser/Desktop/rosbag_2Aug/*.bag'))
-    """
-    for b in bag_files: # we don't assume we are geting them in chronological order
+    bag_files = sorted(glob.glob(opj(bag_files_path,'*.bag')))  # e.g.,'/home/karlzipser/Desktop/rosbag_2Aug'
+    
+    for b in bag_files:
         
         print b
 
@@ -50,11 +54,11 @@ def do_A(bag_files_path,save_pngs=False,png_path='',scale_factor=1.0):
 
         for m in bag.read_messages(topics=['/bair_car/zed/left/image_rect_color']):
             t = round(m.timestamp.to_time(),3)
-            A['left_image'][t] = 'z' #imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
+            A['left_image'][t] = 'z'
 
         for m in bag.read_messages(topics=['/bair_car/zed/right/image_rect_color']):
             t = round(m.timestamp.to_time(),3)
-            A['right_image'][t] = 'z' #imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
+            A['right_image'][t] = 'z'
 
 
     for img in ['left_image','right_image']:
@@ -63,81 +67,51 @@ def do_A(bag_files_path,save_pngs=False,png_path='',scale_factor=1.0):
         for t in sorted_timestamps:
             A[img][t] = ctr
             ctr += 1
-    """
     
-    ctr1 = 0
-    ctr2 = 0
-    A['left_image_folder_number'] = {}
-    for b in bag_files: # we don't assume we are geting them in chronological order
-        print b
-        bag = rosbag.Bag(b)
-        for m in bag.read_messages(topics=['/bair_car/zed/left/image_rect_color']):
-            t = round(m.timestamp.to_time(),3)
-            t_str = "%.3f"%t
-            A['left_image_folder_number'][t] = ctr1
-            img = imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),0.25)
-            unix('mkdir -p ' + opj(bag_files_path,'png/left_image',str(ctr1)),False)
-            imsave(opj(bag_files_path,'png/left_image',str(ctr1),t_str+'.png'), img)
-            ctr2 += 1
-            if ctr2 >= 300:
-                ctr2 = 0
-                ctr1 += 1
+    if save_pngs:
+        ctr1 = 0
+        ctr2 = 0
+        for side in ['left','right']:
+            A[side+'_image_folder_number'] = {}
+            for b in bag_files:
+                print b
+                bag = rosbag.Bag(b)
+                for m in bag.read_messages(topics=['/bair_car/zed/'+side+'/image_rect_color']):
+                    t = round(m.timestamp.to_time(),3)
+                    t_str = "%.3f"%t
+                    A[side+'_image_folder_number'][t] = ctr1
+                    if scale_factor == 1.0:
+                        img = bridge.imgmsg_to_cv2(m[1],"rgb8")
+                    else:
+                        img = imresize(bridge.imgmsg_to_cv2(m[1],"rgb8"),scale_factor)
+                    unix('mkdir -p ' + opj(bag_files_path,'png/'+side+'_image',str(ctr1)),False)
+                    imsave(opj(bag_files_path,'png/'+side+'_image',str(ctr1),t_str+'.png'), img)
+                    ctr2 += 1
+                    if ctr2 >= 300:
+                        ctr2 = 0
+                        ctr1 += 1
     
+    return A
 
-
+def apply_rect_to_img(img,value,min_val,max_val,pos_color,neg_color,relative_height,center=True):
+    pass
 #
 ######################################################################
 
 ######################### binding data to left_image timestamps ######
 #
-def interpolate_single_values(A,topic):
-    interp_dic = {}
-    k,d = get_sorted_keys_and_data(A[topic])
-    for i in range(0,len(k)-1):
-        for j in range(int(k[i]*1000),int(k[i+1]*1000)):
-            v =  (d[i+1]-d[i])/(k[i+1]-k[i]) * (j/1000.-k[i])  + d[i]
-            interp_dic[j/1000.] = v
-    return interp_dic
-
-def interpolate_vector_values(A,topic):
-    interp_dic = {}
-    k,d = get_sorted_keys_and_data(A[topic])
-    d = np.array(d)
-    dim = len(d[0])
-    for i in range(0,len(k)-1):
-        for j in range(int(k[i]*1000),int(k[i+1]*1000)):
-            v = []
-            for u in range(dim):
-                v.append(  (d[i+1,u]-d[i,u])/(k[i+1]-k[i]) * (j/1000.-k[i])  + d[i,u] )
-            interp_dic[j/1000.] = v
-    return interp_dic
-
-def assign_right_image_timestamps(A):
-    interp_dic = {}
-    k,d = get_sorted_keys_and_data(A['right_image'])
-    for i in range(0,len(k)-1):
-        a = int(k[i]*1000)
-        b = int(k[i+1]*1000)
-        c = (a+b)/2
-        for j in range(a,b):
-            if j < c:
-                v = k[i]
-            else:
-                v = k[i+1]
-            interp_dic[j/1000.] = v
-    return interp_dic
 
 def bind_left_image_timestamps_to_data(A):
 
     ms_timestamps = {}
 
-    ms_timestamps['right_image'] = assign_right_image_timestamps(A)
+    ms_timestamps['right_image'] = _assign_right_image_timestamps(A)
 
     for topic in single_value_topics:
-        ms_timestamps[topic] = interpolate_single_values(A,topic)
+        ms_timestamps[topic] = _interpolate_single_values(A,topic)
 
     for topic in vector3_topics:
-        ms_timestamps[topic] = interpolate_vector_values(A,topic)
+        ms_timestamps[topic] = _interpolate_vector_values(A,topic)
 
     left_image_bound_to_data = {}
 
@@ -158,26 +132,57 @@ def bind_left_image_timestamps_to_data(A):
     print error_log
     return left_image_bound_to_data,error_log
 
+
+def _interpolate_single_values(A,topic):
+    interp_dic = {}
+    k,d = get_sorted_keys_and_data(A[topic])
+    for i in range(0,len(k)-1):
+        for j in range(int(k[i]*1000),int(k[i+1]*1000)):
+            v =  (d[i+1]-d[i])/(k[i+1]-k[i]) * (j/1000.-k[i])  + d[i]
+            interp_dic[j/1000.] = v
+    return interp_dic
+
+def _interpolate_vector_values(A,topic):
+    interp_dic = {}
+    k,d = get_sorted_keys_and_data(A[topic])
+    d = np.array(d)
+    dim = len(d[0])
+    for i in range(0,len(k)-1):
+        for j in range(int(k[i]*1000),int(k[i+1]*1000)):
+            v = []
+            for u in range(dim):
+                v.append(  (d[i+1,u]-d[i,u])/(k[i+1]-k[i]) * (j/1000.-k[i])  + d[i,u] )
+            interp_dic[j/1000.] = v
+    return interp_dic
+
+def _assign_right_image_timestamps(A):
+    interp_dic = {}
+    k,d = get_sorted_keys_and_data(A['right_image'])
+    for i in range(0,len(k)-1):
+        a = int(k[i]*1000)
+        b = int(k[i+1]*1000)
+        c = (a+b)/2
+        for j in range(a,b):
+            if j < c:
+                v = k[i]
+            else:
+                v = k[i+1]
+            interp_dic[j/1000.] = v
+    return interp_dic
 #
 ######################################################################
 
 
-######################### diagnostic ####################
+######################### diagnostic graphics ####################
 #
-def get_timestamp_intervals(timestamps,ignore_threshold):
-    d = []
-    for i in range(0,len(timestamps)-1):
-        interval = timestamps[i+1] - timestamps[i]
-        if interval < ignore_threshold:
-            d.append(interval)
-        else:
-            print d2s("!!!WARNING, inverval =",interval,"s. Ignoring this interval!!! ( timestamp =",timestamps[i],")")
-    return d
+def make_timestamp_diagnostic_histograms(A):
+    hist_topics_timestamp_intervals(A,all_topics,0.1)
+    hist_diff_left_right_image_timestamps(A,ignore_threshold=0.1)
 
 def hist_topics_timestamp_intervals(A,topics,ignore_threshold=0.1):
     for s in topics:
         k,d = get_sorted_keys_and_data(A[s])
-        i = get_timestamp_intervals(k,ignore_threshold)
+        i = _get_timestamp_intervals(k,ignore_threshold)
         plt.figure(s)
         hist(i,bins=100)
         plt.title(d2s(s,"intervals"))
@@ -198,8 +203,15 @@ def hist_diff_left_right_image_timestamps(A,ignore_threshold=0.1):
     plt.title("hist_diff_left_right_image_timestamps")
     plt.xlabel("seconds")
 
-def make_timestamp_diagnostic_histograms(A):
-    pass
+def _get_timestamp_intervals(timestamps,ignore_threshold):
+    d = []
+    for i in range(0,len(timestamps)-1):
+        interval = timestamps[i+1] - timestamps[i]
+        if interval < ignore_threshold:
+            d.append(interval)
+        else:
+            print d2s("!!!WARNING, inverval =",interval,"s. Ignoring this interval!!! ( timestamp =",timestamps[i],")")
+    return d
 
 def plot_topics(A):
 
