589dfd39135a614866757a685c0d7bd7b38410db
Karl Zipser <karlzipser@berkeley.edu>
2016-08-07 11:53:31 -0700

    gacp

--- teg1/rosbag_work/learn_rosbag_analysis.py
+++ teg1/rosbag_work/learn_rosbag_analysis.py
@@ -109,7 +109,7 @@ def Preprocess_Bag_Data(bag_files_path,save_pngs=False,scale_factor=1.0,apply_re
     return preprocessed_data,left_image_bound_to_data
     
 
-def apply_rect_to_img(img,value,min_val,max_val,pos_color,neg_color,rel_bar_height,rel_bar_thickness,center=False,reverse=False):
+def apply_rect_to_img(img,value,min_val,max_val,pos_color,neg_color,rel_bar_height,rel_bar_thickness,center=False,reverse=True):
     h,w,d = shape(img)
     p = (value - min_val) / (max_val - 1.0*min_val)
     if reverse:
