4f980741353e8188715818d88192f9a20af9ae93
Karl Zipser <karlzipser@berkeley.edu>
2016-09-10 06:55:46 -0700

    gacp

---
+++ teg1/rosbag_work/get_ros_images_live.py
@@ -0,0 +1,27 @@
+import roslib
+import cv2
+from cv_bridge import CvBridge,CvBridgeError
+import rospy
+from sensor_msgs.msg import Image
+import scipy
+import numpy as np
+
+bridge = CvBridge()
+
+
+rospy.init_node('listener',anonymous=True)
+
+A = 0
+
+def callback(data):
+	global A
+	A += 1
+	print A
+	cimg = bridge.imgmsg_to_cv2(data,"bgr8")
+	cv2.imshow("Image Window",np.flipud(cimg))
+	cv2.waitKey(1)
+
+
+rospy.Subscriber("/bair_car/zed/right/image_rect_color",Image,callback)
+
+
