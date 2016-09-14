351039943558d13b75582fa92969da902411614b
Karl Zipser <karlzipser@berkeley.edu>
2016-09-14 09:04:45 -0700

    gacp

--- teg1/rosbag_work/get_ros_images_live2.py
+++ teg1/rosbag_work/get_ros_images_live2.py
@@ -84,7 +84,7 @@ while True:
 		solver.net.blobs['ZED_data'].data[0,2,:,:] = r0[:,:,1]#/255.0-.5
 		solver.net.blobs['ZED_data'].data[0,3,:,:] = r1[:,:,1]#/255.0-.5
 
-		solver.new.forward()
+		solver.net.forward()
 		cv2.imshow("Left",solver.net.blobs['ZED_data_pool2'].data[0,1,:,:]/255.0)
 		cv2.imshow("Right",solver.net.blobs['ZED_data_pool2'].data[0,3,:,:]/255.0)
 		cv2.waitKey(1)
