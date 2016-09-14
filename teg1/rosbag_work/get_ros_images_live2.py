a5126dac51e7a346683eaae15d313b31cdc1153f
Karl Zipser <karlzipser@berkeley.edu>
2016-09-14 08:13:29 -0700

    gacp

--- teg1/rosbag_work/get_ros_images_live2.py
+++ teg1/rosbag_work/get_ros_images_live2.py
@@ -26,15 +26,21 @@ B = 0
 def right_callback(data):
 	global A
 	A += 1
-	print (A,B)
+	print (A,B,len(left_list),len(right_list))
 	cimg = bridge.imgmsg_to_cv2(data,"bgr8")
-	cv2.imshow("Right",cimg)
+	if len(right_list) > 5:
+		right_list = right_list[-5:]
+	right_list.append(cimg)
+	cv2.imshow("Right",right_list[-1]-left_list[-1])#cimg)
 	cv2.waitKey(1)
 
 def left_callback(data):
 	global B
 	B += 1
 	cimg = bridge.imgmsg_to_cv2(data,"bgr8")
+	if len(left_list) > 5:
+		left_list = left_list[-5:]
+	left_list.append(cimg)
 	#cv2.imshow("Left",cimg)
 	#cv2.waitKey(1)
 
