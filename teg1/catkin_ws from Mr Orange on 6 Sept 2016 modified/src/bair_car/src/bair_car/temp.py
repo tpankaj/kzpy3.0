777fc2b095784707d34f297aa0d825ddee55d922
Karl Zipser <karlzipser@berkeley.edu>
2016-09-16 06:52:34 -0700

    gacp

--- teg1/catkin_ws from Mr Orange on 6 Sept 2016 modified/src/bair_car/src/bair_car/temp.py
+++ teg1/catkin_ws from Mr Orange on 6 Sept 2016 modified/src/bair_car/src/bair_car/temp.py
@@ -1,10 +1,12 @@
 import rospy
 import std_msgs.msg
 
+
 def callback(data):
 	print data.data
-	
+
 rospy.init_node('test',anonymous=True)
 
 cmd_steer_sub = rospy.Subscriber('cmd/steer', std_msgs.msg.Int32, callback)
 
+rospy.spin()
