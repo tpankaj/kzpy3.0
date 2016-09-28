19510417e934aeaecde1d142ea713764908caaf5
Karl Zipser <karlzipser@berkeley.edu>
2016-09-28 15:28:44 -0700

    gacp

---
+++ teg1/rosbag_work/example_of_accessing_data2.py
@@ -0,0 +1,54 @@
+from kzpy3.vis import *
+from kzpy3.teg1.rosbag_work.get_data_from_bag_files3 import *
+
+path = '/media/your_computer/rosbags/bair_car_data'
+path = '/home/karlzipser/Desktop/Old_Desktop/bair_car_rescue/bair_car_data'
+path = opjD('temp_bags_saved')
+
+num_bag_files_to_sample_from_given_run = 10
+num_samples_from_given_bag_file = 3
+
+num_frames = 32
+
+topics = ['state','encoder','steer','motor','gyro','acc','left_right'] # images not included in this list
+
+data_object = Bair_Car_Data(path, num_bag_files_to_sample_from_given_run, num_samples_from_given_bag_file)
+plt.ion()
+
+def apply_rect_to_img(img,value,min_val,max_val,pos_color,neg_color,rel_bar_height,rel_bar_thickness,center=False,reverse=False):
+    h,w,d = shape(img)
+    p = (value - min_val) / (max_val - 1.0*min_val)
+    if reverse:
+        p = 1.0 - p
+    if p > 1:
+        p = 1
+    if p < 0:
+        p = 0
+    wp = int(p*w)
+    bh = int((1-rel_bar_height) * h)
+    bt = int(rel_bar_thickness * h)
+    
+    if center:
+        if wp < w/2:
+            img[(bh-bt/2):(bh+bt/2),(wp):(w/2),:] = neg_color
+        else:
+            img[(bh-bt/2):(bh+bt/2),(w/2):(wp),:] = pos_color
+    else:
+        img[(bh-bt/2):(bh+bt/2),0:wp,:] = pos_color
+
+
+for i in range(10000):
+	a_data_sequence = data_object.get_data(topics, num_frames, num_frames)
+
+	plt.figure('acc')
+	plt.clf()
+	plt.figure('acc')
+
+	plt.plot(a_data_sequence['acc'])
+	plt.plot(a_data_sequence['state'])
+	plt.plot(np.array(a_data_sequence['steer'])/10.)
+	for j in range(num_frames):
+		img = a_data_sequence['left'][j]
+		apply_rect_to_img(img,a_data_sequence['steer'][j],0,99,[1.0],[0.0],0.9,0.1,center=True,reverse=True)
+		mi(img,'left',img_title=str(j))
+		plt.pause(0.00001)
