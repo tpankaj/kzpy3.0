bf89e72895effb32ce2dff1d57accbf30876f319
Karl Zipser <karlzipser@berkeley.edu>
2016-09-30 22:15:04 -0700

    gacp

--- caf3/z3/train.py
+++ caf3/z3/train.py
@@ -13,11 +13,11 @@ USE_GPU = False
 #          SETUP SECTION
 #
 solver_file_path1 = opjh("kzpy3/caf3/z2/solver.prototxt")
-weights_file_path1 = sys.argv[1] #None#opjD('z2_2nd_pass/z2_2nd_pass_iter_30000.caffemodel') #
+weights_file_path1 = opjD("z2/z2.caffemodel") #sys.argv[1] #None#opjD('z2_2nd_pass/z2_2nd_pass_iter_30000.caffemodel') #
 if weights_file_path1 == "None":
 	weights_file_path1 = None
 solver_file_path2 = opjh("kzpy3/caf3/z3/solver.prototxt")
-weights_file_path2 = sys.argv[2] #None#opjD('z2_2nd_pass/z2_2nd_pass_iter_30000.caffemodel') #
+weights_file_path2 = None #sys.argv[2] #None#opjD('z2_2nd_pass/z2_2nd_pass_iter_30000.caffemodel') #
 if weights_file_path2 == "None":
 	weights_file_path2 = None
 #
@@ -34,27 +34,28 @@ def setup_solver(solver_file_path):
 
 
 img = zeros((94,168,3))#,'uint8')
-def load_data_into_model(solver,data):
+def load_data_into_model(solver,data,start,num_steps,flip_left_right):
 	global img
 	if data == 'END' :
 		print """data = 'END':"""
 		return False
 	if 'left' in data:
 		if type(data['left'][0]) == np.ndarray:
-			target_data = data['steer']
-			target_data += data['motor']
+			target_data = []
+			target_data += data['steer'][start:(start+num_steps)]
+			target_data += data['motor'][start:(start+num_steps)]
 
-			if np.random.random() > 0.5:
-				solver.net.blobs['ZED_data_pool2'].data[0,0,:,:] = data['left'][0][:,:]/255.0-.5
-				solver.net.blobs['ZED_data_pool2'].data[0,1,:,:] = data['left'][1][:,:]/255.0-.5
-				solver.net.blobs['ZED_data_pool2'].data[0,2,:,:] = data['right'][0][:,:]/255.0-.5
-				solver.net.blobs['ZED_data_pool2'].data[0,3,:,:] = data['right'][1][:,:]/255.0-.5
+			if flip_left_right: #np.random.random() > 0.5:
+				solver.net.blobs['ZED_data_pool2'].data[0,0,:,:] = data['left'][0+start][:,:]/255.0-.5
+				solver.net.blobs['ZED_data_pool2'].data[0,1,:,:] = data['left'][1+start][:,:]/255.0-.5
+				solver.net.blobs['ZED_data_pool2'].data[0,2,:,:] = data['right'][0+start][:,:]/255.0-.5
+				solver.net.blobs['ZED_data_pool2'].data[0,3,:,:] = data['right'][1+start][:,:]/255.0-.5
 
 			else: # flip left-right
-				solver.net.blobs['ZED_data_pool2'].data[0,0,:,:] = scipy.fliplr(data['left'][0][:,:]/255.0-.5)
-				solver.net.blobs['ZED_data_pool2'].data[0,1,:,:] = scipy.fliplr(data['left'][1][:,:]/255.0-.5)
-				solver.net.blobs['ZED_data_pool2'].data[0,2,:,:] = scipy.fliplr(data['right'][0][:,:]/255.0-.5)
-				solver.net.blobs['ZED_data_pool2'].data[0,3,:,:] = scipy.fliplr(data['right'][1][:,:]/255.0-.5)
+				solver.net.blobs['ZED_data_pool2'].data[0,0,:,:] = scipy.fliplr(data['left'][0+start][:,:]/255.0-.5)
+				solver.net.blobs['ZED_data_pool2'].data[0,1,:,:] = scipy.fliplr(data['left'][1+start][:,:]/255.0-.5)
+				solver.net.blobs['ZED_data_pool2'].data[0,2,:,:] = scipy.fliplr(data['right'][0+start][:,:]/255.0-.5)
+				solver.net.blobs['ZED_data_pool2'].data[0,3,:,:] = scipy.fliplr(data['right'][1+start][:,:]/255.0-.5)
 				for i in range(len(target_data)/2):
 					t = target_data[i]
 					t = t - 49
@@ -85,7 +86,7 @@ def load_data_into_model(solver,data):
 			solver.net.blobs['metadata'].data[0,3,:,:] = Direct
 			solver.net.blobs['metadata'].data[0,4,:,:] = Play
 			solver.net.blobs['metadata'].data[0,5,:,:] = Furtive
-
+			
 			for i in range(len(target_data)):
 				solver.net.blobs['steer_motor_target_data'].data[0,i] = target_data[i]/99.
 
@@ -171,15 +172,53 @@ if weights_file_path2 != None:
 	print "loading " + weights_file_path2
 	solver2.net.copy_from(weights_file_path2)
 
-bair_car_data = Bair_Car_Data(opjD('temp'),1000,100)
+#bair_car_data = Bair_Car_Data(opjD('temp'),1000,100)
 #bair_car_data = Bair_Car_Data(opjD('temp_bags_saved/folder'),1000,100)
+bair_car_data = Bair_Car_Data(opjD('bair_car_data_min'),1000,100)
 
+ctr = 0
+loss = []
+img = zeros((94,168,3))
 
+while True:
 
-data = bair_car_data.get_data(['steer','motor'],32,32)
-assert(load_data_into_model(solver_list1[0],data[0:10]))
-solver1_list[0].net.forward(end='conv2')
-assert(load_data_into_model(solver_list1[1],data[2:12]))
-solver1_list[1].net.forward(end='conv2')
-print 'here!'
+	data = bair_car_data.get_data(['steer','motor'],32,32)
+	flip_left_right = False
+	if np.random.random() < 0.5:
+		flip_left_right = True
+	load_data_into_model(solver1_list[0],data,0,10,flip_left_right)
+	solver1_list[0].net.forward(end='conv2')
+	load_data_into_model(solver1_list[1],data,2,10,flip_left_right)
+	solver1_list[1].net.forward(end='conv2')
+
+	solver2.net.blobs['conv2_PAST'].data[:] = solver1_list[0].net.blobs['conv2'].data[:]
+	solver2.net.blobs['conv2_PRESENT'].data[:] = solver1_list[1].net.blobs['conv2'].data[:]
+	solver2.net.blobs['steer_motor_target_data'].data[:] = solver1_list[1].net.blobs['steer_motor_target_data'].data[:]
+	solver2.step(1)
+
+	a = solver2.net.blobs['steer_motor_target_data'].data[0,:] - solver2.net.blobs['ip2'].data[0,:]
+	loss.append(np.sqrt(a * a).mean())
+	imshow = False
+	datashow = False
+	if np.mod(ctr,10) == 0:
+		imshow = True
+	if np.mod(ctr,50) == 0:
+		datashow = True
+	if datashow:
+		if len(loss) > 1000:
+			print (ctr,np.array(loss[-1000:]).mean())
+		print array_to_int_list(solver2.net.blobs['steer_motor_target_data'].data[0,:][:])
+		print array_to_int_list(solver2.net.blobs['ip2'].data[0,:][:])
+
+	if imshow:
+		img[:,:,0] = solver1_list[1].net.blobs['ZED_data_pool2'].data[0,0,:,:]
+		img += 0.5
+		img *= 255.
+		img[:,:,1] = img[:,:,0]
+		img[:,:,2] = img[:,:,0]
+		cv2.imshow('left',img.astype('uint8'))
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+		    pass
+
+	ctr += 1
 
