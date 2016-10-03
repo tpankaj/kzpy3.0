f1e0f0ceb34d15aca1de787ba40065ff9b63e696
Karl Zipser <karlzipser@berkeley.edu>
2016-10-03 08:21:28 -0700

    gacp

--- teg1/Arduino/under_development/read_serial.py
+++ teg1/Arduino/under_development/read_serial.py
@@ -1,7 +1,7 @@
 #from kzpy3.utils import *
 import serial
 
-ser = serial.Serial('/dev/tty.usbmodem1421',baudrate=115200, timeout=100)
+ser = serial.Serial('/dev/tty.usbmodem14111',baudrate=115200, timeout=100)
 
 while(True):
 	l = ser.readline()
