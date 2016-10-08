374c74138edec1c3e5031bc1ab764b1adb6fc01c
Karl Zipser <karlzipser@berkeley.edu>
2016-10-08 11:26:12 -0700

    gacp

---
+++ teg2/data/preprocess/multi_folder_preprocess.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+
+from kzpy3.teg2.data.preprocess.preprocess_bag_data import *
+from kzpy3.teg2.data.access.get_data_from_bag_files import *
+
+bag_folders = gg(opj(sys.argv[1],'*')) #gg('/home/karlzipser/Desktop/bair_car_data/*')
+
+
+for b in bag_folders[:]:
+        try:
+        	preprocess_bag_data(b)
+        except Exception,e:
+        	print e
+        try:
+            save_grayscale_quarter_bagfolder(b)
+        except Exception,e:
+        	print e
+
+"""
+ save_grayscale_quarter_bagfolder(b)
+ for n in [0,2,3]:
+ 	b = bag_folders[n]
+ 	try:
+ 		preprocess_bag_data(b)
+ 		save_grayscale_quarter_bagfolder(b)
+ 	except:
+ 		pass
+"""
