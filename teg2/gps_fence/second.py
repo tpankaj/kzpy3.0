87dc7e355d90d92da603e03b6cc9763dc7cce972
Karl Zipser <karlzipser@berkeley.edu>
2016-10-23 20:18:41 -0700

    gacp

--- teg2/gps_fence/second.py
+++ teg2/gps_fence/second.py
@@ -1,9 +1,12 @@
+# export ROS_MASTER_URI=http://192.168.43.113:11311
+
+
 from kzpy3.vis import *
 import roslib
 import std_msgs.msg
 import rospy
 
-GPS2_lat = 0
+GPS2_lat = -999.9
 GPS2_long = 0
 GPS2_speed = 0
 GPS2_angle = 0
