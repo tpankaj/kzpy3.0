6f199431f4c086c3e38a3aaa618862218780207c
Karl Zipser <karlzipser@berkeley.edu>
2016-10-29 16:39:49 -0700

    gacp

--- teg2/data/access/get_data_from_bag_files8.py
+++ teg2/data/access/get_data_from_bag_files8.py
@@ -11,6 +11,11 @@ class Bag_Folder:
         if len(gg(left_path)) == 0:
             left_path = opj(path,'.preprocessed','left_image_bound_to_data.pkl')
         self.left_image_bound_to_data = load_obj(left_path)
+        if 'acc' not in an_element(self.left_image_bound_to_data):
+            for ts in self.left_image_bound_to_data:
+                self.left_image_bound_to_data[ts]['acc'] = [0.,9.8,0.]
+        self.data = {}
+        self.data['raw_timestamps'] = sorted(self.left_image_bound_to_data.keys())
         self.good_timestamps_to_raw_timestamps_indicies__dic = {}
         self.img_dic = {}
         self.img_dic['left'] = {}
@@ -24,7 +29,10 @@ class Bag_Folder:
         good_timestamps_set = set(self.left_image_bound_to_data.keys()) & set(self.img_dic['left'].keys())
         bad_timestamps_list = []
         cprint('basic checking . . .','yellow')
-        for ts in good_timestamps_set:
+        for ts in self.data['raw_timestamps']:
+            if not ts in self.img_dic['left']:
+                bad_timestamps_list.append(ts)
+                continue                
             if not self.left_image_bound_to_data[ts]['right_image'] in self.img_dic['right']:
                 bad_timestamps_list.append(ts)
                 continue
@@ -76,6 +84,9 @@ class Bag_Folder:
             if not len(SL[ts]['gyro']) == 3:
                 bad_timestamps_list.append(ts)
                 continue
+            if not len(SL[ts]['acc']) == 3:
+                bad_timestamps_list.append(ts)
+                continue
             #assert('encoder' in SL[ts])
             if not type(SL[ts]['motor']) == float:
                 bad_timestamps_list.append(ts)
@@ -88,17 +99,10 @@ class Bag_Folder:
             if not type(SL[ts]['state']) == float:
                 bad_timestamps_list.append(ts)
                 continue
-            if 'acc' in SL[ts]:
-                #assert(len(SL[ts]['acc']) == 3)
-                if not len(SL[ts]['acc']) == 3:
-                    bad_timestamps_list.append(ts)    
-                    continue
-            else:
-                SL[ts]['acc'] = [0.,0.,0.]
+
 
         if len(bad_timestamps_list) > 0:
-            for bt in bad_timestamps_list:
-                good_timestamps_set.remove(bt)
+            good_timestamps_set -= set(bad_timestamps_list)
             cprint(d2s('Removed bad_timestamps:',len(bad_timestamps_list),'of',len(good_timestamps_set),'total.'),'red')
             #print bad_timestamps_list
         cprint('basic checking done','yellow')
@@ -134,9 +138,9 @@ class Bag_Folder:
             if self.left_image_bound_to_data[ts]['state_one_steps'] < NUM_STATE_ONE_STEPS:
                 bad_timestamps.append(ts)
 
-        self.data = {}
+        
         self.data['good_start_timestamps'] = sorted(list(set(good_timestamps_list) - set(bad_timestamps)))
-        self.data['raw_timestamps'] = sorted(self.left_image_bound_to_data.keys())
+        
         for gts in self.data['good_start_timestamps']:
             raw_index = self.data['raw_timestamps'].index(gts)
             self.good_timestamps_to_raw_timestamps_indicies__dic[gts] = raw_index
@@ -159,6 +163,7 @@ class Bag_Folder:
         self.data['state_one_steps'] = self.elements('state_one_steps')
 
         acc = self.elements('acc')
+        print acc
         self.data['acc_x'] = acc[:,0]
         self.data['acc_z'] = acc[:,1]
         self.data['acc_y'] = acc[:,2]
@@ -175,7 +180,6 @@ class Bag_Folder:
         for e in extremes:
             cprint(d2s("Bag_Folder::__init__", e[0],len(self.data[e[0]])),'blue')
             self.fix_extremes(e[0],e[1],e[2])
-
 
 
 
@@ -212,7 +216,7 @@ class Bag_Folder:
                 data.append(self.left_image_bound_to_data[t][topic])
             else:
                 data.append(-999.999)
-                print "Bag_Folder::elements Warning, data.append(-999.999)"
+                print "Bag_Folder::elements Warning, data.append(-999.999), topic ="+topic
         return np.array(data)
 
 
@@ -351,7 +355,7 @@ def show_data_dic(d):
     t0 = 0
     dt = 0
     for i in range(len(d['steer'])):
-        print i
+        #print i
         if i > 0:
             img_prev = img.copy()
         img = np.zeros((shape(d['right'][0])[0],shape(d['right'][0])[1],3),'uint8')
@@ -372,7 +376,7 @@ def show_data_dic(d):
         apply_rect_to_img(img,d['gyro_x'][i],-60,60,steer_rect_color,steer_rect_color,0.75,0.05,center=True,reverse=False,horizontal=True)        
         
         apply_rect_to_img(img,d['motor'][i],49,99,steer_rect_color,steer_rect_color,0.8,0.05,center=False,reverse=True,horizontal=False)
-        apply_rect_to_img(img,d['encoder'][i],0,10,steer_rect_color,steer_rect_color,0.78,0.05,center=False,reverse=True,horizontal=False)
+        apply_rect_to_img(img,d['encoder'][i],0,15,steer_rect_color,steer_rect_color,0.78,0.05,center=False,reverse=True,horizontal=False)
 
         apply_rect_to_img(img,d['gyro_y'][i],-240,240,steer_rect_color,steer_rect_color,0.2,0.05,center=True,reverse=False,horizontal=False)
         apply_rect_to_img(img,d['acc_y'][i],-48,48,steer_rect_color,steer_rect_color,0.18,0.05,center=True,reverse=False,horizontal=False)
@@ -404,3 +408,128 @@ plt.clf()
 plt.hist(dts,bins=50);
 True
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Bair_Car_Data:
+    """ """
+    def __init__(self, path, to_ignore=[]):
+        self.bag_folders_with_loaded_images = {}
+        self.bag_folders_priority_list = []
+        self.bag_folders_dic = {}
+        self.incremental_index = 0
+        self.ctr = 0
+        bag_folder_paths = sorted(glob.glob(opj(path,'*')))
+        bag_folder_paths_dic = {}
+        for b in bag_folder_paths:
+            bag_folder_paths_dic[b] = True
+            for ig in to_ignore:
+                if ig in b:
+                    bag_folder_paths_dic[b] = False
+                    print "Not using " + b.split('/')[-1]
+        temp = []
+        #print "bag_folder_paths 1: "
+        #print bag_folder_paths
+        for b in bag_folder_paths_dic.keys():
+            if bag_folder_paths_dic[b]:
+                temp.append(b)
+        bag_folder_paths = temp
+        #print "bag_folder_paths: "
+        #print bag_folder_paths
+        self.bag_folders_weighted = []
+        train_preprocessed_bag_folder_path = opjD('train_preprocessed_bag_folder_path')
+        if True:#try:
+            unix('mkdir -p '+train_preprocessed_bag_folder_path)
+            for f in bag_folder_paths:
+                if len(opj(train_preprocessed_bag_folder_path,f.split('/')[-1]+'.pkl')) == 1:
+                    self.bag_folders_dic[f] = load_obj(opj(train_preprocessed_bag_folder_path,f.split('/')[-1]+'.pkl'))
+                    print "loaded "+opj(train_preprocessed_bag_folder_path,f.split('/')[-1]+'.pkl')
+                else
+                    n = len(gg(opj(f,'.preprocessed','*.bag.pkl')))
+                    m = len(gg(opj(f,'.preprocessed','left*')))
+                    #print(f.split('/')[-1],n,m)
+                    if n > 0 and m > 0:
+                        self.bag_folders_dic[f] = Bag_Folder(f)
+                        bpath = opj(train_preprocessed_bag_folder_path,self.bag_folders_dic[f].path.split('/')[-1]+'.pkl')
+                        if len(gg(bpath)) == 0:
+                            print("saveing "+bpath)
+                            save_obj(self.bag_folders_dic[f],bpath)
+                self.bag_folders_with_loaded_images[f] = True
+                for i in range(max(n/10,1)):
+                self.bag_folders_weighted.append(f)
+        if False: #except Exception as e:
+            cprint("Bair_Car_Data::__init__ ********** Exception ******* with "+f,'red')
+            print e.message, e.args
+
+
+    def load_bag_folder_images(self,num_bags_to_load):
+        self.bag_folders_priority_list = self.bag_folders_dic.keys()
+        random.shuffle(self.bag_folders_priority_list)
+        bag_count = 0
+        i = 0
+        for i in range(len(self.bag_folders_priority_list)):
+            b = self.bag_folders_priority_list[i]
+            bag_count += len(self.bag_folders_dic[b].files)
+            if bag_count > num_bags_to_load:
+                break
+        cprint(d2s("\nUsing",i,"bag folders"),'red','on_green')
+        for j in range(len(self.bag_folders_priority_list)-1,0,-1):
+            #print j
+            b = self.bag_folders_priority_list[j]
+            if j <= i:
+                if b in self.bag_folders_with_loaded_images:
+                    cprint("Bair_Car_Data::load_bag_folder_images() already have "+b.split('/')[-1],'blue')
+                    pass
+                else:
+                    #m=memory()
+                    #free_propotion = m['free']/(1.0*m['total'])
+                    #if free_propotion > min_free_proportion: 
+                    self.bag_folders_dic[b].load_all_bag_files()
+                    self.bag_folders_with_loaded_images[b] = 'Loaded'
+                    cprint("Bair_Car_Data::load_bag_folder_images() loaded "+b.split('/')[-1],'green')
+                    #else:
+                    #    cprint("Bair_Car_Data::load_bag_folder_images() didn't load "+b+" because of memory limit.",'blink')
+            else:
+                if b in self.bag_folders_with_loaded_images:
+                    cprint("Bair_Car_Data::load_bag_folder_images() removed "+b.split('/')[-1],'red')
+                    del self.bag_folders_dic[b].img_dic
+                    self.bag_folders_dic[b].img_dic = {}
+                    del self.bag_folders_with_loaded_images[b]
+                    #gc.collect()
+        cprint(d2s("Bair_Car_Data::load_bag_folder_images() self.bag_folders_with_loaded_images (",len(self.bag_folders_with_loaded_images),')'),'yellow','on_blue')
+        for f in self.bag_folders_with_loaded_images:
+            print '\t'+f.split('/')[-1]
+
+    
+
+    def get_data(self,topics=['steer','motor'],num_topic_steps=10,num_image_steps=2):
+        self.bag_folders_with_loaded_images_list = sorted(self.bag_folders_with_loaded_images.keys())
+        t0 = time.time()
+        while True:
+            rc = random.choice(self.bag_folders_with_loaded_images_list)
+            BF = self.bag_folders_dic[rc]
+            BF.incremental_index = np.random.randint(0,len(BF.data['good_start_timestamps']))
+            data = BF.get_data(topics,num_topic_steps,num_image_steps)
+            if data != 'end_of_bag_folder_reached':
+                if self.ctr == 0:
+                    print('self.ctr == 0')
+                if np.mod(self.ctr,1000) == 0:
+                    cprint(self.bag_folders_with_loaded_images_list[self.incremental_index],'magenta')
+                    self.ctr += 1
+                return data        
+            if time.time()-t0 > 60:
+                print "Bair_Car_Data::get_data, timeout"
+                assert(0)
