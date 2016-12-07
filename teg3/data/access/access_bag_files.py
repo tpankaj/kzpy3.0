7ecd14fb3b9dfa948d377ba621f2607b6911ab6d
Karl Zipser <karlzipser@berkeley.edu>
2016-12-07 11:49:07 -0800

    gacp

---
+++ teg3/data/access/access_bag_files.py
@@ -0,0 +1,96 @@
+from kzpy3.vis import *
+import kzpy3.teg3.data.access.Bag_Folder as Bag_Folder
+import kzpy3.teg3.data.access.Bag_File as Bag_File
+from kzpy3.teg3.data.preprocess.preprocess_Bag_Folders import get_preprocess_dir_name_info as get_preprocess_dir_name_info
+import cv2
+
+def load_Bag_Folders(bag_folders_path = opjD('runs')):
+
+	BF_dic = {}
+
+	bag_folders_paths_list = sorted(gg(opj(bag_folders_path,'*')),key=natural_keys)
+
+	for bfp in bag_folders_paths_list:
+
+		run_name = bfp.split('/')[-1]
+
+		preprocessed_dir,left_image_bound_to_data_name = get_preprocess_dir_name_info(bfp)
+
+		if len(gg(opj(bfp,preprocessed_dir,'Bag_Folder.pkl'))) == 1:
+			print('')
+			cprint(opj(run_name,preprocessed_dir,'Bag_Folder.pkl')+' exists, loading it.','yellow','on_red')
+			BF = load_obj(opj(bfp,preprocessed_dir,'Bag_Folder.pkl'))
+		else:
+			if False:
+				BF = Bag_Folder.init(bfp,
+					preprocessed_dir=preprocessed_dir,
+					left_image_bound_to_data_name=left_image_bound_to_data_name,
+					NUM_STATE_ONE_STEPS=10)
+				save_obj(BF,opj(bfp,preprocessed_dir,'Bag_Folder.pkl'))
+			cprint('ERROR!!! '+opj(run_name,preprocessed_dir,'Bag_Folder.pkl')+' does not exist!','yellow','on_red')
+			assert(False)
+
+		if run_name in BF_dic:
+			cprint('ERROR!!! '+run_name+' already in BF_dic!','yellow','on_red')
+			assert(False)
+		BF_dic[run_name] = BF
+
+	return BF_dic
+
+BF_dic = load_Bag_Folders(opjD('runs'))
+
+run_names = BF_dic.keys()
+
+loaded_bag_files_names = []
+
+def bag_file_loader_thread():
+	global loaded_bag_files_names
+	for j in range(100):
+		try:
+			r = random.choice(run_names)
+			BF = BF_dic[r]
+			if 'bag_file_image_data' not in BF:
+				BF['bag_file_image_data'] = {}
+			bf = random.choice(BF['bag_file_num_dic'])
+			if bf in BF['bag_file_image_data']:
+				continue
+			BF['bag_file_image_data'][bf] = Bag_File.load_images(bf)
+			loaded_bag_files_names.append(bf)
+		except Exception as e:
+			cprint("********** Exception ***********************",'red')
+			print(emessage, e.args)
+
+	print("DONE!!!!!!!!!!!!!!!")
+
+
+import threading
+threading.Thread(target=bag_file_loader_thread).start()
+
+"""
+	
+
+for j in range(100):
+	r = random.choice(run_names)
+	BF = BF_dic[r]
+	bf = random.choice(BF['bag_file_num_dic'])
+	bid = Bag_File.load_images(bf)
+	bag_left_timestamps = sorted(bid['left'].keys())
+
+	good_bag_timestamps = list(set(BF['data']['good_start_timestamps']) & set(bag_left_timestamps))
+	
+	cprint(d2s('len(good_bag_timestamps) =',len(good_bag_timestamps)),'blue')
+
+	ts = sorted(bid['left'].keys())
+	for i in range(len(ts)):
+		t = ts[i]
+		#mi(bid['left'][t],'left')
+
+		cv2.imshow('left',cv2.cvtColor(bid['left'][t],cv2.COLOR_RGB2BGR))#.astype('uint8'))
+
+		if cv2.waitKey(3) & 0xFF == ord('q'):
+		    j=1000;break
+	#plt.pause(1.0/60.0)
+
+cv2.destroyAllWindows()
+"""
+;
---
+++ teg3/data/preprocess/preprocess_Bag_Folders.py
@@ -0,0 +1,68 @@
+from kzpy3.vis import *
+import kzpy3.teg3.data.access.Bag_Folder as Bag_Folder
+
+
+
+
+def preprocess_Bag_Folder(bag_folders_path = opjD('runs')):
+	bag_folders_paths_list = sorted(gg(opj(bag_folders_path,'*')),key=natural_keys)
+
+	for bfp in bag_folders_paths_list:
+
+		run_name = bfp.split('/')[-1]
+
+		preprocessed_dir,left_image_bound_to_data_name = get_preprocess_dir_name_info(bfp)
+
+		if len(gg(opj(bfp,preprocessed_dir,'Bag_Folder.pkl'))) == 1:
+			print('')
+			cprint(opj(run_name,preprocessed_dir,'Bag_Folder.pkl')+' exists, loading it.','yellow','on_red')
+			BF = load_obj(opj(bfp,preprocessed_dir,'Bag_Folder.pkl'))
+		else:
+			BF = Bag_Folder.init(bfp,
+				preprocessed_dir=preprocessed_dir,
+				left_image_bound_to_data_name=left_image_bound_to_data_name,
+				NUM_STATE_ONE_STEPS=10)
+			save_obj(BF,opj(bfp,preprocessed_dir,'Bag_Folder.pkl'))
+
+		
+		figure(run_name+' timecourses')
+		plot(BF['data']['raw_timestamps'],100*BF['data']['encoder'],'y')
+		plot(BF['data']['raw_timestamps'],BF['data']['state_one_steps'],'bo-')
+		plot(BF['data']['good_start_timestamps'],zeros(len(BF['data']['good_start_timestamps']))+100,'go')
+		plot(BF['data']['raw_timestamps'],2000*BF['data']['raw_timestamp_deltas'],'r')
+		ylim(0,1000)
+
+		figure(run_name+' raw_timestamp_deltas')
+		rtd = BF['data']['raw_timestamp_deltas'].copy()
+		rtd[rtd>0.08] = 0.08
+		hist(rtd)
+		#plot(BF['data']['raw_timestamps'],100*BF['data']['state'],'r')
+		#plot(BF['data']['raw_timestamps'],100*BF['data']['acc_z'],'r')
+
+		figure(run_name+' scatter')
+		plot(BF['data']['steer'][BF['data']['good_start_indicies']],BF['data']['gyro_x'][BF['data']['good_start_indicies']],'o')
+
+		plt.pause(0.001)
+
+
+
+def get_preprocess_dir_name_info(bfp):
+	# Some tiresome checking for which preprocessed data version to use.
+	if len(gg(opj(bfp,'.preprocessed2'))) == 1:
+		if len(gg(opj(bfp,'.preprocessed2','left_image_bound_to_data.pkl'))) == 1:
+			preprocessed_dir = '.preprocessed2'
+			left_image_bound_to_data_name = 'left_image_bound_to_data.pkl'		
+		else:
+			assert(False)
+	elif len(gg(opj(bfp,'.preprocessed'))) == 1:
+		if len(gg(opj(bfp,'.preprocessed','left_image_bound_to_data.pkl'))) == 1:
+			preprocessed_dir = '.preprocessed'
+			left_image_bound_to_data_name = 'left_image_bound_to_data.pkl'		
+		elif len(gg(opj(bfp,'.preprocessed','left_image_bound_to_data2.pkl'))) == 1:
+			preprocessed_dir = '.preprocessed'
+			left_image_bound_to_data_name = 'left_image_bound_to_data2.pkl'
+		else:
+			assert(False)
+	else:
+		assert(False)
+	return preprocessed_dir,left_image_bound_to_data_name
