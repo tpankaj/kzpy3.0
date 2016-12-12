5dde6fb7d74cb39b05af5f334aa8405cb91caed7
Karl Zipser <karlzipser@berkeley.edu>
2016-12-11 20:27:21 -0800

    gacp

--- teg2/bdd_car_versions/bdd_car_GPS2/bair_car/nodes/get_ros_images_live2_z2_TX1.py
+++ teg2/bdd_car_versions/bdd_car_GPS2/bair_car/nodes/get_ros_images_live2_z2_TX1.py
@@ -138,10 +138,10 @@ from computer_name import *
 from kzpy3.teg2.global_run_params import *
 
 t0 = time.time()
-time_step = Timer(2)
+time_step = Timer(1)
 while not rospy.is_shutdown():
 	if state in [3,5,6,7]:
-		if state in [3] and state_transition_time_s > 1.0:
+		if True: #state in [3] and state_transition_time_s > 1.0:
 
 			if len(left_list) > 4:
 				l0 = left_list[-2]
