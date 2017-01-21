f70af6d99416c6c48f3b896ae4148266bd26a6a5
Karl Zipser <karlzipser@berkeley.edu>
2017-01-20 19:11:21 -0800

    gacp

--- caf6/z2_color_big/visualize.py
+++ caf6/z2_color_big/visualize.py
@@ -5,7 +5,7 @@ os.chdir(home_path) # this is for the sake of the train_val.prototxt
 ########################################################
 #          SETUP SECTION
 #
-solver_file_path = opjh("kzpy3/caf6/z2_color/solver.prototxt")
+solver_file_path = opjh("kzpy3/caf6/z2_color_big/solver.prototxt")
 #
 ########################################################
 
@@ -18,13 +18,13 @@ def setup_solver():
 	return solver
 
 solver = setup_solver()
-weights_file_path = opjD('z2_color')
-#weights_file_path = '/home/karlzipser/Desktop/z2_color/z2_color_0_a_iter_11900000.caffemodel'
-weights_file_path = most_recent_file_in_folder(weights_file_path,['z2_color_0_a','caffemodel'])
+weights_file_path = opjD('z2_color_big')
+#weights_file_path = '/home/karlzipser/Desktop/z2_color_big/z2_color_big'
+weights_file_path = most_recent_file_in_folder(weights_file_path,['z2_color_big','caffemodel'])
 solver.net.copy_from(weights_file_path)
 plt.ion()
 
-for n in range(96):
+for n in range(2*96):
 	width = 11
 	img = np.zeros((2*width+7,2*width+7,3))
 	for c in range(3):
--- caf6/z2_color_small/visualize.py
+++ caf6/z2_color_small/visualize.py
@@ -5,7 +5,7 @@ os.chdir(home_path) # this is for the sake of the train_val.prototxt
 ########################################################
 #          SETUP SECTION
 #
-solver_file_path = opjh("kzpy3/caf6/z2_color/solver.prototxt")
+solver_file_path = opjh("kzpy3/caf6/z2_color_small/solver.prototxt")
 #
 ########################################################
 
@@ -19,8 +19,8 @@ def setup_solver():
 
 solver = setup_solver()
 weights_file_path = opjD('z2_color')
-#weights_file_path = '/home/karlzipser/Desktop/z2_color/z2_color_0_a_iter_11900000.caffemodel'
-weights_file_path = most_recent_file_in_folder(weights_file_path,['z2_color_0_a','caffemodel'])
+weights_file_path = '/home/karlzipser/Desktop/z2_color_small'
+weights_file_path = most_recent_file_in_folder(weights_file_path,['z2_color_small','caffemodel'])
 solver.net.copy_from(weights_file_path)
 plt.ion()
 
