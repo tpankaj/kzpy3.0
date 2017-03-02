78825f208478bd424f01080d1244a85d84a46767
Karl Zipser <karlzipser@berkeley.edu>
2017-03-01 16:55:36 -0800

    gacp

--- teg2/car_run_params.py
+++ teg2/car_run_params.py
@@ -34,12 +34,12 @@ weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more.caffemodel")
 
 weights_file_path = opjh("kzpy3/caf5/z2_color/z2_color.caffemodel")
 weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more_2.caffemodel")
-weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more_3.caffemodel")
+
 weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_4000000.caffemodel")
 weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_6000000.caffemodel")
 weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_10900000.caffemodel")
 #weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_8000000.caffemodel")
-
+weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more_3.caffemodel")
 verbose = False
 
 steer_gain = 1.0
