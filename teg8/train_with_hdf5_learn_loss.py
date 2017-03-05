af8e6b24ed6bff1f521bbd082b106b4563dc9541
Karl Zipser <karlzipser@berkeley.edu>
2017-03-05 22:54:23 +0100

    gacp

--- teg8/train_with_hdf5_learn_loss.py
+++ teg8/train_with_hdf5_learn_loss.py
@@ -58,12 +58,12 @@ if True:
 	ctr_low = -1 # These counter keep track of position in segment lists, and when to reshuffle.
 	ctr_high = -1
 
-if False:
+if True:
 	solver_name = opjh('kzpy3/caf7/z2_color/solver_loss.prototxt')
 	solver = setup_solver(solver_name)
 	#weights_file_path = opjh('kzpy3/caf5/z2_color/z2_color.caffemodel')
 	#solver.net.copy_from(weights_file_path)
-	#cprint('Loaded weights from '+weights_file_path)
+	cprint('Loaded weights from '+weights_file_path)
 	N_FRAMES = 2 # how many timesteps with images.
 	N_STEPS = 10 # how many timestamps with non-image data
 
