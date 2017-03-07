ea49fc9008af0d773eedbd67e889ce9afc7b9126
Karl Zipser <karlzipser@berkeley.edu>
2017-03-07 18:40:15 +0100

    gacp

--- teg8/temp.py
+++ teg8/temp.py
@@ -30,43 +30,123 @@ weights_file_path =  opjD(fname(model_path)) #opjD('z2_color_trained_12_15_2016'
 #
 ##############################################################################
 #
+"""
+layer {
+  name: "left_pool_pool_scale"
+  type: "Scale"
+  bottom: "left_pool_pool"
+  top: "left_pool_pool_scale"
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  scale_param {
+    filler {
+      value: 0.003921    }
+    bias_term: true
+    bias_filler {
+      value: -0.5
+    }
+  }
+}
+"""
+
+"""
+layer {
+  name: "left_pool_pool_scale"
+  type: "Scale"
+  bottom: "left_pool_pool"
+  top: "left_pool_pool_scale"
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  scale_param {
+    filler {
+      value: 0.003921    }
+    bias_term: true
+    bias_filler {
+      value: -0.5
+    }
+  }
+}
+"""
 train_val_lst = [
+
 	d2s('#',model_path),
 	d2s('#',time_str('Pretty')),
 
-	dummy('left_pool2',(1,12,94,168)),
-	dummy('right_pool2',(1,12,94,168)),
+	dummy('left',(1,12,376,672)),
+	pool("left","AVE",3,2,0),
+	pool("left_pool","AVE",3,2,0),
 
-	conv("conv1_left",'left_pool2',48,1,3,2,0,"gaussian",std='0.00001'),
+	dummy('right',(1,6,376,672)),
+	pool("right","AVE",3,2,0),
+	pool("right_pool","AVE",3,2,0),
+	#dummy('left_pool2',(1,12,94,168)),
+	#dummy('right_pool2',(1,12,94,168)),
+
+	dummy('steer_motor_target_data',(1,60)),
+	#dummy('motor_target_data',(1,30)),
+	dummy('metadata',(1,6,11,20)),
+	dummy('past_steer',(1,30,11,20)),
+
+	dummy('past_motor',(1,30,11,20)),
+	dummy('xy_gradients',(1,2,11,20)),
+
+	conv("conv1_left",'left_pool_pool',48,1,3,2,0,"gaussian",std='0.00001'),
 	relu("conv1_left"),
 	pool("conv1_left","MAX",3,2,0),
-	drop('conv1_left_pool',0.0),
 
-	conv("conv1_right",'right_pool2',48,1,3,2,0,"gaussian",std='0.00001'),
+
+	conv("conv1_right",'right_pool_pool',48,1,3,2,0,"gaussian",std='0.00001'),
 	relu("conv1_right"),
 	pool("conv1_right","MAX",3,2,0),
-	drop('conv1_right_pool',0.0),
 
 	concat('conv1_concat',["conv1_left","conv1_right"],1),
 	concat('conv1_pool_concat',["conv1_left_pool","conv1_right_pool"],1),
 
-	conv("conv2a_bi",'conv1_concat',48,1,3,2,0,"gaussian",std='0.00001'),
+	conv("conv2a_bi",'conv1_concat',96,1,3,2,0,"gaussian",std='0.1'),
 	relu("conv2a_bi"),
 	pool("conv2a_bi","MAX",3,2,0),
 
-	conv("conv2b_bi",'conv1_pool_concat',48,1,3,2,0,"gaussian",std='0.00001'),
-	relu("conv2b_bi")
-]
-"""
-	concat('conv2_concat',["conv2a_bi","conv2b_bi"],1),
+	conv("conv2b_bi",'conv1_pool_concat',96,1,3,2,0,"gaussian",std='0.1'),
+	relu("conv2b_bi"),
 
-	conv("conv2_1x1",'conv2_concat',16,1,1,1,0,"gaussian",std='0.00001'),
+
+
+
+	drop('past_steer',0.5),
+	drop('past_motor',0.5),
+
+	concat('conv2_concat',["conv2a_bi_pool","conv2b_bi","metadata","past_steer","past_motor","xy_gradients"],1),
+
+	conv("conv2_1x1",'conv2_concat',96,1,1,1,0,"gaussian",std='0.1'),
 	relu("conv2_1x1"),
-	conv("conv2_3x3",'conv2_concat',16,1,1,1,0,"gaussian",std='0.00001'),
+	conv("conv2_3x3",'conv2_concat',96,1,3,1,[1,1],"gaussian",std='0.1'),
 	relu("conv2_3x3"),
+
+	concat('conv2_final_concat',["conv2_1x1","conv2_3x3"],1),
+
+	conv("conv3",'conv2_final_concat',96,1,3,1,[1,1],"gaussian",std='0.1'),
+	relu("conv3"),
+	pool("conv3","MAX",3,2,0),
+
+	ip("ip1","conv3_pool",512,"xavier",std=0),
+	relu('ip1'),
+	drop('ip1',0.0),
+	ip("ip2","ip1",60,"xavier",std=0),
+	euclidean("euclidean","steer_motor_target_data","ip2")
 	
 ]
-"""
 #
 ##############################################################################
 ##############################################################################
