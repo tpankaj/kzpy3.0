0d569e768ca8b891220fec282cf969021a3c11c1
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2017-03-11 13:13:47 -0800

    Put gradient clipping in solver for lstm_only
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- teg7/train_with_hdf5_lstm_only.py
+++ teg7/train_with_hdf5_lstm_only.py
@@ -63,8 +63,8 @@ if True:
 	#weights_file_path = opjh('kzpy3/caf5/z2_color/z2_color.caffemodel')
 	#solver.net.copy_from(weights_file_path)
 	#cprint('Loaded weights from '+weights_file_path)
-	N_FRAMES = 2 # how many timesteps with images.
-	N_STEPS = 2 # how many timestamps with non-image data
+	N_FRAMES = 5 # how many timesteps with images.
+	N_STEPS = 5 # how many timestamps with non-image data
 	ignore=[reject_run,left,out1_in2] # runs with these labels are ignored
 	require_one=[] # at least one of this type of run lable is required
 	print_timer = Timer(5)
@@ -134,7 +134,8 @@ if True:
                 #for i in range(6, 97):
                 #        solver.net.blobs['metadata'].data[0,i,:,:] = 0.0
                 solver.net.blobs['clip'].data[0,0] = 1
-                solver.net.blobs['clip'].data[1,0] = 0
+                for i in range(1, N_STEPS):
+                        solver.net.blobs['clip'].data[i,0] = 0
                 for step in range(N_STEPS):
                         solver.net.blobs['steer_motor_target_data'].data[step, 0, 0] = data['steer'][(-step - 1)]/99
                         solver.net.blobs['steer_motor_target_data'].data[step, 0, 1] = data['motor'][(-step - 1)]/99
