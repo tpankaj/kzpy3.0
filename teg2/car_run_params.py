54629a281bf78713125ac478e2183fdc70eeac86
Karl Zipser <karlzipser@berkeley.edu>
2017-03-21 07:58:22 -0700

    gacp

--- teg2/car_run_params.py
+++ teg2/car_run_params.py
@@ -27,21 +27,19 @@ Play = 0.
 Furtive = 0.
 Caf = 1.0
 Racing = 0.0
-Location = 'Smyth_tape'
+Location = 'test'
 
 solver_file_path = opjh("kzpy3/caf5/z2_color/solver_live.prototxt")
 weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more.caffemodel")
-
-
 weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more_2.caffemodel")
-
 weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_4000000.caffemodel")
 weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_6000000.caffemodel")
 weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_10900000.caffemodel")
-#weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_8000000.caffemodel")
+weights_file_path = opjh("kzpy3/caf7/z2_color/z2_color_state_1_5_6_7_iter_8000000.caffemodel")
 weights_file_path = opjh("kzpy3/caf6/z2_color_more/z2_color_more_3.caffemodel")
 weights_file_path = opjh("kzpy3/caf7/z2_color/solver_state_1_5_6_7_plus_extra_Smyth_racing_iter_400000.caffemodel")
 weights_file_path = opjh("kzpy3/caf5/z2_color/z2_color.caffemodel")
+
 verbose = False
 
 steer_gain = 1.0
